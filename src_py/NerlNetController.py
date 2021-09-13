1d2156b0ba9ceee5fd46212ce3b7c599fabc2ae4
zivmo99 <xtziv9walla.co.il>
2021-09-13 06:56:09 -0700

    config and comments

--- src_py/NerlNetController.py
+++ src_py/NerlNetController.py
@@ -3,13 +3,25 @@ import requests
 
 def updateCSVtrain():
 	# r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,w2,./input/inputShort.csv")
-	r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/traininput.csv")
+	r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/traininputShuffled.csv")
+	#r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/traininput.csv")
+	#r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/train70percent.csv")
+	#r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/train70percentShuffled.csv")
+	
 	# r = requests.post('http://192.168.0.107:8080/updateCSV',data = "s1,w1,w2rr,./input/shuffled-input99.csv")
 	# r2 = requests.post('http://192.168.0.107:8080/updateCSV',data = "source2,worker5,worker7, ... ,./input/input4.csv")...
 	print(r.text)
 
 def updateCSVpredict():
-	r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/input9p.csv")
+	r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/predict10noLabels.csv")
+	#r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/predict30percentNoLabels.csv")
+	# r = requests.post('http://192.168.0.107:8080/updateCSV',data = "s1,w1,w2,./input/shuffled-input99.csv")
+	# r2 = requests.post('http://192.168.0.107:8080/updateCSV',data = "source2,worker5,worker7, ... ,./input/input4.csv")...
+	print(r.text)
+
+def updateCSVpredict1():
+	#r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/predict10noLabels.csv")
+	r = requests.post('http://127.0.0.1:8080/updateCSV',data = "s1,w1,./input/predict30percentNoLabels.csv")
 	# r = requests.post('http://192.168.0.107:8080/updateCSV',data = "s1,w1,w2,./input/shuffled-input99.csv")
 	# r2 = requests.post('http://192.168.0.107:8080/updateCSV',data = "source2,worker5,worker7, ... ,./input/input4.csv")...
 	print(r.text)
@@ -44,6 +56,8 @@ while 1:
 		updateCSVtrain()	
 	elif action == "init2":
 		updateCSVpredict()
+	elif action == "init3":
+		updateCSVpredict1()
 	elif action == "train":
 		clientsTraining()
 
