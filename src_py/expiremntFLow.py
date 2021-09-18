c1c34ff2106a8efef5f5cea5e2285e2d7f02110b
zivmo99 <xtziv9walla.co.il>
2021-09-18 07:29:49 -0700

    a

--- src_py/expiremntFLow.py
+++ src_py/expiremntFLow.py
@@ -1,15 +1,15 @@
 def initTrain():
-    new_list = [('http://127.0.0.1:8080/updateCSV', "s1,w1,./input/inputShort.csv"),
-                ('http://127.0.0.1:8080/updateCSV', "s2,w2,./input/inputShort2.csv"),
+    new_list = [('http://127.0.0.1:8080/updateCSV', "s1,w1,./input/skin_nonskin.csv"),
+                ('http://127.0.0.1:8080/updateCSV', "s1,w2,./input/skin_nonskin.csv"),
                 ('http://127.0.0.1:8080/clientsTraining', "")]
     return new_list
 
 def startCasting():
-    new_list = [('http://127.0.0.1:8080/startCasting', "s1,s2")]
+    new_list = [('http://127.0.0.1:8080/startCasting', "s1")]
     return new_list
 
 def initPredict():
-    new_list = [('http://127.0.0.1:8080/updateCSV', "s1,w1,./input/input9p.csv"),
-                ('http://127.0.0.1:8080/updateCSV', "s2,w2,./input/input9p.csv"),
+    new_list = [('http://127.0.0.1:8080/updateCSV', "s1,w1,./input/skin_nonskin_predict.csv"),
+                ('http://127.0.0.1:8080/updateCSV', "s1,w2,./input/skin_nonskin_predict.csv"),
                 ('http://127.0.0.1:8080/clientsPredict', "")]
     return new_list
