fa5cc8592edb0f7ca4a16e73b9db8506f89d0057
zivmo99 <xtziv9walla.co.il>
2021-09-26 15:11:45 -0700

    built script mv folders

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 #Builds 
 
 git clone https://github.com/zivmo99/inputDataFiles.git
+mv inputDataFiles/RunOrWalkPredictNolabelsNormalized_splitted/ ./src_erl/Communication_Layer/http_Nerlserver/input/
+mv inputDataFiles/RunOrWalkTrainNormalized_splitted/ ./src_erl/Communication_Layer/http_Nerlserver/input/
 
 cd src_py
 python3 BuildScript.py cpp=True
