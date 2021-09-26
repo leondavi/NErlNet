6b59192d62de0f58ba816c33203601d562f8258a
aslan <aslana@post.bgu.ac.il>
2021-09-27 00:47:33 +0300

    Revert "WIP"
    This reverts commit 9b2943e196dd244aaa11d5eca76bf9836a201068.

--- src_py/apiServer.py
+++ src_py/apiServer.py
@@ -14,7 +14,7 @@ sys.path.append(os.getcwd() + '/src_py')
 DEFAULT_PORT = 8095
 
 
-def server(port=DEFAULT_PORT):
+def server(port):
     thread = Thread(target=run, args=(port,))
     thread.start()
     time.sleep(2)
@@ -45,12 +45,15 @@ def analyze(arr):
 
 
 def readfile():
-    solution_path = "../src_erl/Communication_Layer/http_Nerlserver/output/w1"
+    solution_path = "w1.txt"
 
     sequence_array = []
     with open(solution_path, 'r') as f:
         for line in f.readlines():
             sequence_array.append(float(line))
+    # print(map(float, sequence_array))
+
+    print(sequence_array)
     analyze(sequence_array)
 
 
@@ -101,63 +104,12 @@ def getLoosVector():
 def getDistanceVector():
     pass
 
-def calculateAccuracy(resultPath, inputPath):
-    resFile = open(resultPath, "r")
-    inputFile = open(inputPath, "r")
-    roundNumbersFile = open(resultPath + "round", "a")
-
-    for y in resFile:
-        ResLineWithIndex1 = y.split()
-        ResLine1 = ResLineWithIndex1[1]
-        numbers = ResLine1.split(",")
-        numberslen = len(numbers) - 1
-        roundNumbersFile.write(ResLineWithIndex1[0] + " ")
-        for number, item in enumerate(numbers):
-            if number == numberslen:
-                roundedNumber = round(float(item))
-                roundNumbersFile.write(str(roundedNumber))
-            else:
-                roundedNumber = round(float(item))
-                roundNumbersFile.write(str(roundedNumber) + ",")
-
-        roundNumbersFile.write("\n")
-
-    roundNumbersFile.close()
-    resFile.close()
-    resRoundFile = open(resultPath + "round", "r")
-    Lines = inputFile.readlines()
-    total = 0
-    correct = 0
-    for x in resRoundFile:
-        splitted = x.split()
-        Result = splitted[1]
-        index = int(splitted[0]) - 1
-        inputRes = Lines[index].rstrip()
-        if Result == inputRes:
-            # print("correct!")
-            total += 1
-            correct += 1
-        else:
-            # print("wrong!")
-            total += 1
-
-    print("Accuracy:", str(correct) + "/" + str(total), "=", str((correct / total) * 100), "%")
-    inputFile.close()
-    resRoundFile.close()
-
-
 
 if __name__ == "__main__":
     from sys import argv
 
-    # NerlnetPyAPI.settings.lenOfRequests = 6
-    # port = DEFAULT_PORT
-    # server(port)
-    # initTrain()
-    # startCasting()
-    # startPredict()
-    # startCasting()
-    # time.sleep(1)
-    readfile()
-    calculateAccuracy("../src_erl/Communication_Layer/http_Nerlserver/output/predictRunOrWalkPredictNolabels_splitted",
-                      "../src_erl/Communication_Layer/http_Nerlserver/output/RunOrWalkPredictResults.csv")
+    NerlnetPyAPI.settings.lenOfRequests = 6
+    port = DEFAULT_PORT
+    server(port)
+    initTrain()
+    # analyze()
--- src_py/expiremntFLow.py
+++ src_py/expiremntFLow.py
@@ -1,15 +1,14 @@
-def initTrainStack():
+def initTrain():
     new_list = [('http://127.0.0.1:8080/updateCSV', "s1,w1,./input/RunOrWalkTrain_splitted/"),
                 ('http://127.0.0.1:8080/clientsTraining', "")]
     return new_list
 
-def startCastingStack():
+def startCasting():
     # startCasting bode: "source_name1,source_2,....,numberOfSamplesToSend"
-    new_list = [('http://127.0.0.1:8080/startCasting', "s1")]
+    new_list = [('http://127.0.0.1:8080/startCasting', "s1,100000")]
     return new_list
 
 def initPredict():
     new_list = [('http://127.0.0.1:8080/updateCSV', "s1,w1,./input/RunOrWalkPredictNolabels_splitted/"),
                 ('http://127.0.0.1:8080/clientsPredict', "")]
     return new_list
-
