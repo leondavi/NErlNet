8370d49c38ebe24f24242923d9f47c963168a386
zivmo99 <xtziv9walla.co.il>
2021-09-26 22:59:07 -0700

    Merge branch 'apiServer' of github.com:leondavi/NErlNet into l3 and build script update

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 #Builds 
 
 git clone https://github.com/zivmo99/inputDataFiles.git
-mv inputDataFiles/RunOrWalkPredictNolabelsNormalized_splitted/ ./src_erl/Communication_Layer/http_Nerlserver/input/
-mv inputDataFiles/RunOrWalkTrainNormalized_splitted/ ./src_erl/Communication_Layer/http_Nerlserver/input/
+mv inputDataFiles/RunOrWalkPredictNolabels_splitted/ ./src_erl/Communication_Layer/http_Nerlserver/input/
+mv inputDataFiles/RunOrWalkTrain_splitted/ ./src_erl/Communication_Layer/http_Nerlserver/input/
 
 cd src_py
 python3 BuildScript.py cpp=True
