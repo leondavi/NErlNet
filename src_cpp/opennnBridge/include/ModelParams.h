afd6085af887600708f997a4b02433624233802f
evgenyan95 <evgenyan@post.bgu.ac.il>
2021-10-28 18:47:03 +0300

    niftest(creat model) work from erlang

--- src_cpp/opennnBridge/include/ModelParams.h
+++ src_cpp/opennnBridge/include/ModelParams.h
@@ -12,21 +12,32 @@ public:
  	std::shared_ptr<std::vector<int>> activationList, std::shared_ptr<std::vector<int>> layersSizes,
     std::shared_ptr<std::vector<int>> layersTypes);
 
-	int GetOptimizer() { return _optimizer; }
-	
+	int GetOptimizer() { return _optimizer; };
+	unsigned long GetModelId() { return _modelId; };
+    double GetLearningRate() { return _learningRate; };
+    int GetModelType() { return _modelType; };
 	std::shared_ptr<std::vector<int>> GetAcvtivationList() { return _activationList; };
     std::shared_ptr<std::vector<int>> GetLayersSizes() { return _layersSizes; };
     std::shared_ptr<std::vector<int>> GetLayersTypes() { return _layersTypes; };
-    int GetModelType() { return _modelType; };
+    
+
+    void setOptimizer(int x){this->_optimizer = x; };
+    void setModelId(unsigned long x){this->_modelId = x; };
+    void setLearningRate(double x){this->_learningRate = x; };
+    void setModelType(int x){this->_modelType = x; };
+    void setAcvtivationList(std::shared_ptr<std::vector<int>>  x){this->_activationList = x; };
+    void setLayersSizes(std::shared_ptr<std::vector<int>>  x){this->_layersSizes = x; };
+    void setLayersTypes(std::shared_ptr<std::vector<int>>  x){this->_layersTypes = x; };
 
 
     int _optimizer;
     unsigned long _modelId;
     double _learningRate;
+    int _modelType;
     std::shared_ptr<std::vector<int>> _activationList; //shared pointers   std::shared_ptr<std::vector> _activationListPtr; 
     std::shared_ptr<std::vector<int>> _layersSizes; //TODO try to use shared pointers
     std::shared_ptr<std::vector<int>> _layersTypes;
-    int _modelType;
+    
 
 private:
 
@@ -49,15 +60,26 @@ public:
     TrainParams(int rows, int col, int labels, unsigned long mid,std::shared_ptr<std::vector<double>> data_label_mat, 
 			  ErlNifTid tid, ErlNifPid pid);
     
-
+    int GetRows() { return _rows; };
+    int GetCol() { return _col; };
+    int GetLabels() { return _labels; };
+    unsigned long GetOptimizer() { return _mid; };
+    ErlNifTid GetTid() { return _tid; };
+    ErlNifPid GetPid() { return _pid; };
     std::shared_ptr<std::vector<double>> GetDataLabelMat() { return _data_label_mat; };
+    
+    void setRows(int x){this->_rows = x; };
+    void setCol(int x){this->_col = x; };
+    void setLabels(unsigned long x){this->_labels = x; };
+    void setTid(ErlNifTid x){this->_tid = x; };
+    void setPid(ErlNifPid x){this->_pid = x; };
+    void setPid(std::shared_ptr<std::vector<double>> x){this->_data_label_mat = x; };
 
     int _rows, _col, _labels;
-    unsigned long _mid;
-    std::shared_ptr<std::vector<double>> _data_label_mat;
+    unsigned long _mid; 
     ErlNifTid _tid;
     ErlNifPid _pid;
-
+    std::shared_ptr<std::vector<double>> _data_label_mat;
 private:
 
 };
--- src_erl/niftest.erl
+++ src_erl/niftest.erl
@@ -1,11 +1,18 @@
 -module(niftest).
 
--export([init/0, hello/0]).
+-export([init/0,hello/1]).
 
 -on_load(init/0).
 
 init() ->
-      erlang:load_nif("/home/evgeny/work_test/NErlNet/build/libnerlnet", 0).
+      io:format("reached init"),
+      erlang:load_nif("/home/evgeny/work_test/NErlNet/build/libnerlnet", 0),
+      io:format("after init").
+%jello() ->
+      % io:format("hello"),
+      %hello("hello").
+      %erlang:hello().
+      % erlang:nif_error("NIF library not loaded").
 
-hello() ->
-      erlang:nif_error("NIF library not loaded").
+hello(Integer) when is_integer(Integer) ->
+      exit(nif_library_not_loaded).
