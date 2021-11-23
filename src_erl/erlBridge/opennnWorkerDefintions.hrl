df434158aa5646c93b6f76816fa9a844f444f36d
ddleon <ddleon@outlook.co.il>
2021-11-23 10:14:40 -0800

    change and add new files

---
+++ src_erl/erlBridge/opennnWorkerDefintions.hrl
@@ -0,0 +1,7 @@
+
+
+getEnumMapActivationFunction() -> #{enum_threshold => 1, enum_SymmetricThreshold => 2, c=> 4, "a" => 1, "b" => 2, "c" => 4},.
+
+getEnumMapActivationFunction() -> #{Threshold => 1, enum_symmetric_threshold = 2 , Logistic = 3 , HyperbolicTangent = 4 ,
+                         Linear = 5 , RectifiedLinear = 6 , ExponentialLinear = 7 , ScaledExponentialLinear = 8 ,
+                         SoftPlus = 9 , SoftSign = 10 , HardSigmoid = 11 , Binary = 12 , Competitive = 14 , Softmax = 15 }.
