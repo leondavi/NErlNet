259c153300d96c71cd70623416742eb1aac49700
David Leon <tal.kapelnik@gmail.com>
2021-12-05 11:53:28 +0200

    Architecture and communication layer are no loner in the same json

--- src_py/expiremntFLow.py
+++ src_py/expiremntFLow.py
@@ -1,12 +1,11 @@
 def initTrainStack():
     new_list = [('http://127.0.0.1:8080/updateCSV', "s1,w1,./input/RunOrWalkTrain_splitted/"),
-                ('http://127.0.0.1:8080/updateCSV', "s2,w2,./input/RunOrWalkTrain_splitted/"),
                 ('http://127.0.0.1:8080/clientsTraining', "")]
     return new_list
 
 def startCastingStack():
     # startCasting bode: "source_name1,source_2,....,numberOfSamplesToSend"
-    new_list = [('http://127.0.0.1:8080/startCasting', "s1,s2")]
+    new_list = [('http://127.0.0.1:8080/startCasting', "s1")]
     return new_list
 
 def initPredictStack():
