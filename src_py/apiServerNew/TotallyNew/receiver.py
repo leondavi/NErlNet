aef762674dbc4d7e0b92e1a5e416a2178f93d463
david <ddleon@outlook.co.il>
2021-12-06 10:21:46 -0800

    [APIServer] Skeleton and Transmitter example

--- src_py/apiServerNew/TotallyNew/receiver.py
+++ src_py/apiServerNew/TotallyNew/receiver.py
@@ -1,16 +1,10 @@
 from flask import Flask
 from flask_restful import Api, Resource
-import globalVars2 as globe
-import redis
-from rq import Queue
-import queue
+from globalVars2 import *
+from flask import Flask, Api
 
 receiver = Flask(__name__)
 api = Api(receiver)
-
-def runReceiver():
-    print("Starting receiver server...")
-    receiver.run(debug=True, threaded=True, port=8095)
 
 class test(Resource):
     def post(self):
@@ -24,5 +18,3 @@ class testQueue(Resource):
 api.add_resource(test, "/test")
 api.add_resource(testQueue, "/testQueue")
 
-if __name__ == "__main__":
-    runReceiver()
