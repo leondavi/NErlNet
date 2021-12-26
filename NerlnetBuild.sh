0843fadcd7048c297b4fbca2ec3a026ad5cf71d8
david <ddleon@outlook.co.il>
2021-12-26 09:32:29 +0200

    [NerlnetBuild] update script with instructions for first time rebar3 installation

--- NerlnetBuild.sh
+++ NerlnetBuild.sh
@@ -14,6 +14,8 @@ REBAR3_FILE=src_erl/rebar3/rebar3
 
 if [ -f "$REBAR3_FILE" ]; then
 	echo "rebar3 is installed, location: $REBAR3_FILE"
+	echo "Please run the following command from Nerlnet library root folder (or install rebar3 to usr/local/bin):"
+	echo " sudo ln -s `pwd`/src_erl/rebar3/rebar3 /usr/local/bin/rebar3"
 else 
 	echo "rebar3 Builder"
 	cd src_erl/rebar3
