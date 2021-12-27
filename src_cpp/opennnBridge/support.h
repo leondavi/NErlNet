ae81a0c8537ee15b9fb56d664ac856e6b26cd0c0
David Leon <tal.kapelnik@gmail.com>
2021-12-27 13:16:41 +0200

    +support

---
+++ src_cpp/opennnBridge/support.h
@@ -0,0 +1,19 @@
+#pragma once 
+
+#include <string>
+#include <iostream>
+
+#define VERBOSE_NONE 0
+#define VERBOSE_LOW 1
+#define VERBOSE_MEDIUM 2
+#define VERBOSE_HIGH 3
+
+#define VERBOSITY_LEVEL VERBOSE_LOW
+
+
+const std::string opennnBridgeLogPrefix = "[OPENNN_BRIDGE] ";
+
+void printOpennnBridgeLog(char* logStr)
+{
+    std::cout<<opennnBridgeLogPrefix<<logStr<<std::endl;
+}
