2867870d70cac221a920697aea81bcc2fdedd91d
david <ddleon@outlook.co.il>
2021-12-28 01:43:41 +0200

    change build rebar missing color

--- NerlnetBuild.sh
+++ NerlnetBuild.sh
@@ -14,8 +14,9 @@ REBAR3_FILE=src_erl/rebar3/rebar3
 
 if [ -f "$REBAR3_FILE" ]; then
 	echo "rebar3 is installed, location: $REBAR3_FILE"
-	echo "Please run the following command from Nerlnet library root folder (or install rebar3 to usr/local/bin):"
-	echo " sudo ln -s `pwd`/src_erl/rebar3/rebar3 /usr/local/bin/rebar3"
+	echo "$(tput setaf 1) Please run the following command from Nerlnet library root folder (or install rebar3 to usr/local/bin): $(tput sgr 0)"
+	echo "$(tput setaf 1) sudo ln -s `pwd`/src_erl/rebar3/rebar3 /usr/local/bin/rebar3 $(tput sgr 0)"
+	echo " "
 else 
 	echo "rebar3 Builder"
 	cd src_erl/rebar3
