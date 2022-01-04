b63d5b844bcce4fbd4c45b0c2eafd7acda1b1478
dordor7 <dormailbox@gmail.com>
2022-01-04 08:57:09 -0800

    Last commit before meeting 4/1/22

--- src_py/apiServer/test.py
+++ src_py/apiServer/test.py
@@ -1,12 +1,15 @@
-from transmitter import Transmitter
+#from transmitter import Transmitter
+import os
 
 
-defAddress = 'https://httpbin.org/post' #httpbin is a website aimed for testing HTTP requests
-baseReceiverAddress = 'http://127.0.0.1:8095'
+#defAddress = 'https://httpbin.org/post' #httpbin is a website aimed for testing HTTP requests
+#baseReceiverAddress = 'http://127.0.0.1:8095'
 
-trans = Transmitter()
+#trans = Transmitter()
 
-trans.testPost(baseReceiverAddress + '/test', 0)
+#trans.testPost(baseReceiverAddress + '/test', 0)
+
+print(os.getcwd())
 
 
 
