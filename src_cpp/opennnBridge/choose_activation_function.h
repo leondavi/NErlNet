5f10def6a2fb964a70146878472cffd0a2bdfe9a
david <ddleon@outlook.co.il>
2022-05-08 22:42:51 +0300

    [OpenNN] latest master version
    - Fix name spaces
    - Open logarithm scaling method option (compilation passed)

--- src_cpp/opennnBridge/choose_activation_function.h
+++ src_cpp/opennnBridge/choose_activation_function.h
@@ -28,91 +28,91 @@ void chooseActivationFunction(std::shared_ptr<OpenNN::NeuralNetwork> neural_netw
          
 
          //Pooling
-         if( (neural_network->get_trainable_layers_pointers()(i))->get_type() == 3 ) //"Pooling"
+         if( (neural_network->get_trainable_layers_pointers()(i))->get_type() == Layer::Type::Pooling ) //"Pooling"
          {
             if(activations_functions(i) == E_Pooling_Method_NoPooling){
-                  dynamic_cast<PoolingLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_pooling_method(PoolingLayer::NoPooling);
+                  dynamic_cast<PoolingLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_pooling_method(PoolingLayer::PoolingMethod::NoPooling);
             }
 
             if(activations_functions(i) == E_Pooling_Method_MaxPooling){
-                  dynamic_cast<PoolingLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_pooling_method(PoolingLayer::MaxPooling);
+                  dynamic_cast<PoolingLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_pooling_method(PoolingLayer::PoolingMethod::MaxPooling);
             }
             
             if(activations_functions(i) == E_Pooling_Method_AveragePooling){
-                  dynamic_cast<PoolingLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_pooling_method(PoolingLayer::AveragePooling);
+                  dynamic_cast<PoolingLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_pooling_method(PoolingLayer::PoolingMethod::AveragePooling);
             }
          }
 
         
          //Perceptron + LongShortTermMemory + Recurrent + Convolutional
-         if( ((neural_network->get_trainable_layers_pointers()(i))->get_type() == 2) //"Perceptron"
-             || ((neural_network->get_trainable_layers_pointers()(i))->get_type() == 5) //"LongShortTermMemory"
-             || ((neural_network->get_trainable_layers_pointers()(i))->get_type() == 6 ) //"Recurrent"
-             || ( (neural_network->get_trainable_layers_pointers()(i))->get_type() == 1 )) //"Convolutional"
+         if( ((neural_network->get_trainable_layers_pointers()(i))->get_type() == Layer::Type::Perceptron) 
+             || ((neural_network->get_trainable_layers_pointers()(i))->get_type() == Layer::Type::LongShortTermMemory) 
+             || ((neural_network->get_trainable_layers_pointers()(i))->get_type() == Layer::Type::Recurrent ) 
+             || ( (neural_network->get_trainable_layers_pointers()(i))->get_type() == Layer::Type::Convolutional )) 
          {
             if(activations_functions(i) == E_AF_Threshold){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::Threshold);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::Threshold);
             }
 
             if(activations_functions(i) == E_AF_SymmetricThreshold){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::SymmetricThreshold);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::SymmetricThreshold);
             }
 
             if(activations_functions(i) == E_AF_Logistic){ 
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::Logistic);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::Logistic);
             }
 
             if(activations_functions(i) == E_AF_HyperbolicTangent){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::HyperbolicTangent);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::HyperbolicTangent);
             }
 
             if(activations_functions(i) == E_AF_Linear){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::Linear);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::Linear);
             }
 
             if(activations_functions(i) == E_AF_RectifiedLinear){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::RectifiedLinear);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::RectifiedLinear);
             }
 
             if(activations_functions(i) == E_AF_ExponentialLinear){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ExponentialLinear);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::ExponentialLinear);
             }
 
             if(activations_functions(i) == E_AF_ScaledExponentialLinear){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ScaledExponentialLinear);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::ScaledExponentialLinear);
             }
 
             if(activations_functions(i) == E_AF_SoftPlus){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::SoftPlus);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::SoftPlus);
             }
  
             if(activations_functions(i) == E_AF_SoftSign){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::SoftSign);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::SoftSign);
             }
 
             if(activations_functions(i) == E_AF_HardSigmoid){
-                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::HardSigmoid);
+                  dynamic_cast<PerceptronLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(PerceptronLayer::ActivationFunction::HardSigmoid);
             }
          }
 
 
          //Probabilistic
-         if((neural_network->get_trainable_layers_pointers()(i))->get_type() == 4) //"Probabilistic"
+         if((neural_network->get_trainable_layers_pointers()(i))->get_type() == Layer::Type::Probabilistic) //"Probabilistic"
          {
             if(activations_functions(i) == PAF_Binary){
-                  dynamic_cast<ProbabilisticLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(ProbabilisticLayer::Binary);
+                  dynamic_cast<ProbabilisticLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(ProbabilisticLayer::ActivationFunction::Binary);
             }
 
             if(activations_functions(i) == PAF_Logistic){  //problem with Logistic ,the compiler dont like it
-                 dynamic_cast<ProbabilisticLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(ProbabilisticLayer::Logistic);
+                 dynamic_cast<ProbabilisticLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(ProbabilisticLayer::ActivationFunction::Logistic);
             }
 
             if(activations_functions(i) == PAF_Competitive){
-                  dynamic_cast<ProbabilisticLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(ProbabilisticLayer::Competitive);
+                  dynamic_cast<ProbabilisticLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(ProbabilisticLayer::ActivationFunction::Competitive);
             }
 
             if(activations_functions(i) == PAF_Softmax){
-                  dynamic_cast<ProbabilisticLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(ProbabilisticLayer::Softmax);
+                  dynamic_cast<ProbabilisticLayer*>(neural_network->get_trainable_layers_pointers()(i))->set_activation_function(ProbabilisticLayer::ActivationFunction::Softmax);
             }
 
          }
