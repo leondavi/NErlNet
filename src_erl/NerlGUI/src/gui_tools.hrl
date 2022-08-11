817e722c7364d8956441d97b9ef26eb9b10c1256
halfway258 <harancoh@gmail.com>
2022-08-11 15:04:54 +0300

    tile

--- src_erl/NerlGUI/src/gui_tools.hrl
+++ src_erl/NerlGUI/src/gui_tools.hrl
@@ -4,8 +4,8 @@
 
 -define(PADDING_W, 10).
 -define(PADDING_H, 20).
--define(BUTTON_W, 200).
--define(BUTTON_H, 200).
--define(BUTTON_SIZE(Mult), {size, {round(?BUTTON_W*Mult), round(?BUTTON_H*Mult)}}).
+-define(TILE_W, 200).
+-define(TILE_H, 200).
+-define(BUTTON_SIZE(Mult), {size, {round(?TILE_W*Mult), round(?TILE_H*Mult)}}).
 -define(BUTTON_LOC(Row, Col), 
-    {pos, {round((Col+1) * ?PADDING_H + Col * ?BUTTON_H), round((Row+1) * ?PADDING_W + Row * ?BUTTON_W)}}).
+    {pos, {round((Col+1) * ?PADDING_H + Col * ?TILE_H), round((Row+1) * ?PADDING_W + Row * ?TILE_W)}}).
