d15d51f1c81235039c2a1ed42e620998438db7c0
halfway258 <harancoh@gmail.com>
2022-08-16 18:02:13 +0300

    comm

--- src_erl/NerlGUI/src/devScreen.erl
+++ src_erl/NerlGUI/src/devScreen.erl
@@ -26,23 +26,23 @@ init([Parent, PPID]) ->
     wxFont:setPointSize(Font, ?FONT_SIZE),
     wxFrame:setFont(DevFrame, Font),
 
-    wxStaticText:new(DevFrame, 701, "MainServer:", [?BUTTON_SIZE(1), ?BUTTON_LOC(0, 0)]),
+    wxStaticText:new(DevFrame, 710, "MainServer:", [?BUTTON_SIZE(1), ?BUTTON_LOC(0, 0)]),
 
     ServerStatsButton = wxButton:new(DevFrame, 711, [{label, "Main Server Status"}, ?BUTTON_SIZE(1), ?BUTTON_LOC(0.1,0)]), 
     wxButton:connect(ServerStatsButton, command_button_clicked, []),
+    io:format("ServerButton is:, ~p~n", [ServerStatsButton]),
 
     PPID ! {getGraph, self()},
 
-    wxStaticText:new(DevFrame, 702, "Routers:", [?BUTTON_SIZE(1), ?BUTTON_LOC(0, 1)]),
+    wxStaticText:new(DevFrame, 720, "Routers:", [?BUTTON_SIZE(1), ?BUTTON_LOC(0, 1)]),
     %add button for each router
 
-    wxStaticText:new(DevFrame, 703, "Workers:", [?BUTTON_SIZE(1), ?BUTTON_LOC(0, 2)]),
+    wxStaticText:new(DevFrame, 730, "Clients:", [?BUTTON_SIZE(1), ?BUTTON_LOC(0, 2)]),
     %add button for each router
 
-    wxStaticText:new(DevFrame, 704, "Sources:", [?BUTTON_SIZE(1), ?BUTTON_LOC(0, 3)]),
+    wxStaticText:new(DevFrame, 740, "Sources:", [?BUTTON_SIZE(1), ?BUTTON_LOC(0, 3)]),
     %add button for each router
 
-    wxFrame:show(DevFrame),
     {DevFrame, #state{ppid = PPID, frame = DevFrame}}.
 
 
@@ -58,7 +58,7 @@ handle_event(Event, State) ->
         {wxClose, close_window} -> exit(normal);    %destroy(State#state.frame);
         _Button ->
             case ID of
-                ?SERVER_ID ->       serverScreen:new(State#state.frame, "");
+                ?SERVER_ID ->   serverScreen:new(State#state.frame, "");
                 Other ->        io:format("Got event with ID=~p~n",[Other])
             end
         end,
@@ -66,22 +66,45 @@ handle_event(Event, State) ->
     {noreply, State}.
 
 handle_info(Info, State)->
-    io:format("Got mes:~p~n",[Info]),
-    NerlGraph = Info,
+    io:format("Got mes: ~p~n",[Info]),
+    {Action, Data} = Info,
+    NewState = 
+    case Action of
+        show -> wxFrame:show(State#state.frame), State;
+        graphObj -> 
+            NerlGraph = deserialize(Data),
+            add_graph_buttons(State#state.frame, NerlGraph),
+            self() ! {show, self()},
+            State#state{nerlGraph = NerlGraph}
+    end,
+    {noreply, NewState}.
 
-    NerlEntities = [digraph:vertex(NerlGraph, V) || V <- digraph:vertices(NerlGraph)--["serverAPI, mainServer", "nerlGUI"]],
+add_graph_buttons(Frame, NerlGraph)->
+    Verts = digraph:vertices(NerlGraph)--["serverAPI, mainServer", "nerlGUI"],
 
-    % Routers = [digraph:vertex(NerlGraph, V) || V <- digraph:vertices(NerlGraph)--["serverAPI, mainServer", "nerlGUI"], lists:member($r, V)],
-    % Workers = [digraph:vertex(NerlGraph, V) || V <- digraph:vertices(NerlGraph)--["serverAPI, mainServer", "nerlGUI"], lists:member($w, V)],
-    % Sources = [digraph:vertex(NerlGraph, V) || V <- digraph:vertices(NerlGraph)--["serverAPI, mainServer", "nerlGUI"], lists:member($s, V)],
+    Routers = [digraph:vertex(NerlGraph, V) || V <- Verts, lists:member($r, V)],
+    Clients = [digraph:vertex(NerlGraph, V) || V <- Verts, lists:member($c, V)],
+    Sources = [digraph:vertex(NerlGraph, V) || V <- Verts, lists:member($s, V)],
+    % io:format("Rbuttons are: ~p", [RButtons]),
+    % io:format("Wbuttons are: ~p", [CButtons]),
+    % io:format("Sbuttons are: ~p", [SButtons]),
 
-    % %need to sequence the entities and give them all different ids
+    % Id = hd(lists:reverse(Name))-48
+    
+    RButtons = [wxButton:new(Frame, 720 + hd(lists:reverse(Name))-48, [{label, Name}, ?BUTTON_SIZE(1), ?BUTTON_LOC(0.1 + hd(lists:reverse(Name))-48,1)]) || {Name, Label} <- Routers],
+    CButtons = [wxButton:new(Frame, 730 + hd(lists:reverse(Name))-48, [{label, Name}, ?BUTTON_SIZE(1), ?BUTTON_LOC(0.1 + hd(lists:reverse(Name))-48,2)]) || {Name, Label} <- Clients],
+    SButtons = [wxButton:new(Frame, 740 + hd(lists:reverse(Name))-48, [{label, Name}, ?BUTTON_SIZE(1), ?BUTTON_LOC(0.1 + hd(lists:reverse(Name))-48,3)]) || {Name, Label} <- Sources].
+    
 
-    % RButtons = [wxButton:new(State#state.frame, Id, [{label, Name}, ?BUTTON_SIZE(1), ?BUTTON_LOC(0.1,0)]) || {Name, Label}<-Routers]
-
-
-    {noreply, State}.
-
-
-add_dev_button(_Frame, _Dev)->
-    todo.
+deserialize({VL, EL, NL, B}) ->       
+    DG = {digraph, V, E, N, B} = case B of 
+       true -> digraph:new();
+       false -> digraph:new([acyclic])
+    end,
+    ets:delete_all_objects(V),
+    ets:delete_all_objects(E),
+    ets:delete_all_objects(N),
+    ets:insert(V, VL),
+    ets:insert(E, EL),
+    ets:insert(N, NL),
+    DG.
