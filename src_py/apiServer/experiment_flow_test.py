bf6d4332c0a77485ec3f431b1c8845a8f86f0bc8
David Leon <ddleon@outlook.co.il>
2022-10-29 21:39:58 +0300

    [PY] update experiment flow test

--- src_py/apiServer/experiment_flow_test.py
+++ src_py/apiServer/experiment_flow_test.py
@@ -15,3 +15,7 @@ api_server_instance = ApiServer()
 
 api_server_instance.initialization(arch_json , connmap_json, exp_flow_json)
 api_server_instance.sendJsonsToDevices()
+
+api_server_instance.train()
+api_server_instance.predict()
+api_server_instance.statistics()
