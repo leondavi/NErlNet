0337adcfdcee9a7442143d7f47649ecfd3b61c2d
haran <harancoh@gmail.com>
2022-11-17 16:52:21 +0200

    full workflow works

--- src_py/apiServer/experiment_flow_test.py
+++ src_py/apiServer/experiment_flow_test.py
@@ -1,17 +1,17 @@
 
-from jsonDirParser import JsonDirParser
 from apiServer import *
 from experiment import *
 
-json_dir_parser = JsonDirParser()
-json_dir_parser.print_lists()
-json_dir_parser.select_arch_connmap_experiment()
-print()
-print(json_dir_parser.get_user_selection_files())
-
-arch_json , connmap_json, exp_flow_json = json_dir_parser.get_user_selection_jsons()
-
 api_server_instance = ApiServer()
+
+api_server_instance.showJsons()
+
+api_server_instance.selectJsons()
+print()
+print(api_server_instance.getUserJsons())
+
+arch_json , connmap_json, exp_flow_json = api_server_instance.getUserJsons()
+
 
 api_server_instance.initialization(arch_json , connmap_json, exp_flow_json)
 api_server_instance.sendJsonsToDevices()
