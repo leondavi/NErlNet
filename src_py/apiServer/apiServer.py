89cea27a7bb0e9cf2860ef86dafcba1818b0d18f
haran <harancoh@gmail.com>
2022-12-08 15:46:45 +0200

    generalized json pathing

--- src_py/apiServer/apiServer.py
+++ src_py/apiServer/apiServer.py
@@ -63,9 +63,6 @@ Please change the 'host' and 'port' values for the 'serverAPI' key in the archit
         # Send the content of jsonPath to each devices:
         print("\nSending JSON paths to devices...")
 
-        # archAddress = globe.content[0][:-1]
-        # connMapAddress = globe.content[1][:-1]
-
         # Jsons found in NErlNet/inputJsonFiles/{JSON_TYPE}/files.... for entities in src_erl/Comm_layer/http_nerl/src to reach them, they must go up 3 dirs
         archAddress , connMapAddress, exp_flow_json = self.getUserJsons()
         [JsonsPath, archPath] = archAddress.split("/NErlNet")
