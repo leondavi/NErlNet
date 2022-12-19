d2973d0b5011c5f365b5acc5fd2b05b77224e265
haran <harancoh@gmail.com>
2022-12-19 14:15:55 +0200

    init json file attempt

--- src_erl/Communication_Layer/http_Nerlserver/src/jsonHandler.erl
+++ src_erl/Communication_Layer/http_Nerlserver/src/jsonHandler.erl
@@ -18,15 +18,16 @@
 %From erlang(maybe for debug):
 %%httpc:request(post,{"http://localhost:8484/updateJsonPath", [],"application/x-www-form-urlencoded","../../../jsonPath"}, [], []).
 init(Req0, [ApplicationPid]) ->
-  _Scheme = cowboy_req:scheme(Req0),
-  _Host = cowboy_req:host(Req0),
-  _Port = cowboy_req:port(Req0),
-  _Path = cowboy_req:path(Req0),
-  _Qs = cowboy_req:qs(Req0),
+  % _Scheme = cowboy_req:scheme(Req0),
+  % _Host = cowboy_req:host(Req0),
+  % _Port = cowboy_req:port(Req0),
+  % _Path = cowboy_req:path(Req0),
+  % _Qs = cowboy_req:qs(Req0),
   {ok,Body,_} = cowboy_req:read_body(Req0),
-  [ArchitectureAdderess,CommunicationMapAdderess] = re:split(binary_to_list(Body),"#",[{return,list}]),
-  io:format("Body at json Handler: ~p,~n sending to pid: ~p~n", [ArchitectureAdderess,CommunicationMapAdderess]),
+  % [ArchitectureAdderess,CommunicationMapAdderess] = re:split(binary_to_list(Body),"#",[{return,list}]),
+  % io:format("Body at json Handler: ~p,~n sending to pid: ~p~n", [ArchitectureAdderess,CommunicationMapAdderess]),
 
+  io:format("got files: ~p~n",[binary_to_list(Body)]),
   %Notify the application that python is ready and send the addreses received in this http request:
   ApplicationPid ! {jsonAddress,{ArchitectureAdderess,CommunicationMapAdderess}},
   
