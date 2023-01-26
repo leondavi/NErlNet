4419fd994557274fb41ab451fe27021fe79af67e
Haran Cohen <harancoh@gmail.com>
2023-01-26 11:20:56 +0200

    Update apiServer.py

--- src_py/apiServer/apiServer.py
+++ src_py/apiServer/apiServer.py
@@ -161,7 +161,7 @@ Please change the 'host' and 'port' values for the 'serverAPI' key in the archit
         return expResults
    
     def sendDataToSources(self, phase):
-        print("***********NOT READY************")
+        print("***********IN TESTING************")
         print("\nSending data to sources")
         # <num of sources> Acks for updateCSV():
         globe.pendingAcks += len(globe.components.sources) 
