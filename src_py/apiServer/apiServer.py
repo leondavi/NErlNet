3527b64774bdc37d91b58e9224858cf93358b0ca
halfway258 <harancoh@gmail.com>
2023-04-19 16:24:24 +0300

    help and rasp alignment

--- src_py/apiServer/apiServer.py
+++ src_py/apiServer/apiServer.py
@@ -34,11 +34,11 @@ class ApiServer():
         print(
 """
 __________NERLNET CHECKLIST__________
-0. Make sure data and jsons in correct places
+0. Make sure data and jsons in correct folder, and jsons include the correct paths
 1. Run Jupyter in virtual env: source <venv>/bin/activate
             
 ____________API COMMANDS_____________
-        Setting experiment:
+==========Setting experiment========
 -showJsons():                       shows available arch / conn / exp layouts
 -selectJsons():                     get input from user for arch / conn / exp selection
 -setJsons(arch, conn, exp):         set layout in code
@@ -46,16 +46,16 @@ ____________API COMMANDS_____________
 -initialization(arch, conn, exp):   set up server for a NerlNet run
 -sendJsonsToDevices():              send each NerlNet device the arch / conn jsons to init entities on it
 -sendDataToSources(phase):          phase can be "training" / "prediction". send the experiment data to sources (currently happens in beggining of train/predict)
-        Starting Run:
+========Running experiment==========
 -train():                           start training phase
 -predict():                         start prediction phase
 -contPhase(phase):                  send another `Batch_size` of a phase (must be called after initial train/predict)
-        Get Info:
+==========Experiment info===========
 -print_saved_experiments()          prints saved experiments and their number for statistics later
 -plot_loss(ExpNum)                  saves and shows the loss over time of the chosen experiment
 -accuracy_matrix(ExpNum)            shows a graphic for the confusion matrix. Also returns: [TruePos, TrueNeg, FalsePos, FalseNeg]
 -communication_stats()              prints the communication statistics of the current network.
--statistics():                      get specific statistics of experiment (lossFunc graph, accuracy, etc...)
+-statistics():                      get specific statistics of experiment (lossFunc graph, accuracy, etc...) DEPRECATED
 
 _____GLOBAL VARIABLES / CONSTANTS_____
 pendingAcks:                        makes sure API command reaches all relevant entities (wait for pending acks)
