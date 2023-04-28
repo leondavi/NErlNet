ca68c0ae7c1d2577082f9920b0fc1665473d2b2a
leondavi <ddleon@outlook.com>
2023-04-29 02:57:06 +0300

    Encode/Decode - Working - more checks are needed

--- src_erl/erlBridge/nerlTensor.hrl
+++ src_erl/erlBridge/nerlTensor.hrl
@@ -1,5 +1,5 @@
 -define (LIST_GROUP_NERLTENSOR_TYPE, [erl_float, erl_int]).
--define (LIST_BINARY_GROUP_NERLTENSOR_TYPE,[double]).% [float,double,int32,int16]
+-define (LIST_BINARY_GROUP_NERLTENSOR_TYPE,[float,double,int32,int16]).
 
 % bytes_str: list of chars in Erlang and std::string in NIF
 % float32/double: list of floats in Erlang
