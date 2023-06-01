d47aae30ea3e19b2a8d4c508d830725d1f6cbc14
David <ddleon@outlook.co.il>
2023-06-02 01:48:07 +0300

    Remove redundant files + Fix CI issue (#182)
    * Cleaning Depracated
    
    * Cause CI issue
    
    * Fix CI issue

--- src_py/Utilities/nerlnetInit.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-for x in range(2,254):
-	print ('http://192.168.1.' + str(x) + ':8484/updateJsonPath')
-	#response = requests.post('http://192.168.1.' + str(x) + ':8484/updateJsonPath', data='../../../jsonPath')
