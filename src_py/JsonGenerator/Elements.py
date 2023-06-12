a640a9919473deb18620b99507e3eb2e4d2b54de
David <ddleon@outlook.co.il>
2023-06-12 16:01:47 +0300

    [JSONGEN] WIP init json generator project (#187)
    * [JSONGEN] WIP init json generator project
    
    * [JSONGEN] WIP
    
    * [JSONGEN] WIP
    
    * [JSONGEN] WIP add worker window dialog

---
+++ src_py/JsonGenerator/Elements.py
@@ -0,0 +1,17 @@
+
+
+class NerlGUI():
+    def __init__():
+        pass
+
+class ApiServer():
+    def __init__():
+        pass
+
+class MainServer():
+    def __init__():
+        pass
+
+class GenericFields():
+    def __init__():
+        pass
