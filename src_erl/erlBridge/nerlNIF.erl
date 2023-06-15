87f2fcf5effa11bca6be3469e7e971c08907e968
halfway258 <harancoh@gmail.com>
2023-06-15 15:14:50 +0300

    nif load test

--- src_erl/erlBridge/nerlNIF.erl
+++ src_erl/erlBridge/nerlNIF.erl
@@ -2,7 +2,7 @@
 -include_lib("kernel/include/logger.hrl").
 -include("nerlTensor.hrl").
 
--export([init/0,create_nif/6,train_nif/6,call_to_train/6,predict_nif/3,call_to_predict/6,get_weights_nif/1,printTensor/2]).
+-export([init/0,nif_preload/0,create_nif/6,train_nif/6,call_to_train/6,predict_nif/3,call_to_predict/6,get_weights_nif/1,printTensor/2]).
 -export([call_to_get_weights/1,call_to_set_weights/2]).
 -export([decode_nif/2, nerltensor_binary_decode/2]).
 -export([encode_nif/2, nerltensor_encode/5, nerltensor_conversion/2, get_all_binary_types/0, get_all_nerltensor_list_types/0]).
@@ -21,6 +21,8 @@ init() ->
       NELNET_LIB_PATH = ?NERLNET_PATH++?BUILD_TYPE_RELEASE++"/"++?NERLNET_LIB,
       RES = erlang:load_nif(NELNET_LIB_PATH, 0),
       RES.
+
+nif_preload() -> done.
 
 % ModelID - Unique ID of the neural network model 
 % ModelType - E.g. Regression, Classification 
