42b7f850c51112b728542e8baeb938fc925cc7b2
leondavi <ddleon@outlook.co.il>
2023-06-15 22:17:25 +0300

    [WIP]

--- src_py/JsonGenerator/WinWorkerDialog.py
+++ src_py/JsonGenerator/WinWorkerDialog.py
@@ -48,8 +48,8 @@ ModelTypeMapping = {
     "fed-server": 9
 }
 
-def count_str_list_elements(list_str):
-    return len(list_str.split(','))
+def count_str_list_elements(list_str : str):
+    return len(list_str.split(',')) if list_str else 0
 
 def pretty_print_dict(d):#define d
     pretty_dict = ''  #take empty string
@@ -62,11 +62,13 @@ def combo_list_editable_handler(window, event, values, map, editable_list, selec
         if values[codes_key]:
             editable_list = values[codes_key]
         editable_list = editable_list + "," if editable_list else editable_list
-        editable_list += str(map[values[selection_key]])
-        window[codes_key].update(editable_list)
+        if values[selection_key]:
+            editable_list += str(map[values[selection_key]])
+            window[codes_key].update(editable_list)
     elif event == clear_butt_key:
         editable_list = ""
         window[codes_key].update(editable_list)
+    return editable_list
 
 def WinWorkerDialog():
     
@@ -74,9 +76,9 @@ def WinWorkerDialog():
                                [sg.Text("Layers Sizes: Comma separated list, # of neurons in a layer, E.g, 100,80,40,5,1")],
                                [sg.InputText(key="-LAYERS-SIZES-INPUT-"), sg.Text("(0)",key="-NUM-OF-LAYERS-SIZES-")],
                                [sg.Text("List of layers types:"), sg.Combo(list(LayerTypeMap.keys()),key='-LAYER-TYPE-SELECTION-'), sg.Button("Add",key="-LAYER-TYPE-SELECTION-ADD-"), sg.Button("Help",key="-LAYER-TYPE-HELP-"),sg.Button("Clear",key="-LAYER-TYPE-SELECTION-CLEAR-")],
-                               [sg.InputText(key="-LAYER-TYPE-CODES-INPUT-"), sg.Text("(0)",key="-NUM-OF-LAYERS-TYPES-")],
+                               [sg.InputText(key="-LAYER-TYPE-CODES-INPUT-"), sg.Text("(0)",key="-NUM-OF-LAYERS-TYPES-",enable_events=True)],
                                [sg.Text("Activation functions:"), sg.Combo(list(ActivationFunctionsMap.keys()),key='-ACTIVATION-LAYER-SELECTION-'), sg.Button("Add",key="-ACTIVATION-LAYER-SELECTION-ADD-"), sg.Button("Help",key="-ACTIVATION-LAYER-HELP-"), sg.Button("Clear",key="-ACTIVATION-LAYER-SELECTION-CLEAR-")],
-                               [sg.InputText(key="-ACTIVATION-CODES-INPUT-"), sg.Text("(0)",key="-NUM-OF-LAYERS-ACTIVATIONS-")]]
+                               [sg.InputText(key="-ACTIVATION-CODES-INPUT-"), sg.Text("(0)",key="-NUM-OF-LAYERS-ACTIVATIONS-",enable_events=True)]]
     WorkerDefinitionsFrame = sg.Frame("Model Definitions",layout=WorkerDefinitionsLayout)
 
     WorkerFileLayout = [[sg.Text("Select xo file output directory"),sg.In(enable_events=True ,key="-XO-FILE-CHOSEN-DIRECTORY", expand_x=True), sg.FolderBrowse()],
@@ -86,6 +88,7 @@ def WinWorkerDialog():
     WorkerWindow  = sg.Window(title="Worker", layout=[[sg.Text(f'New Worker Generator')],[WorkerDefinitionsFrame],[WorkerFileFrame]],modal=True, keep_on_top=True)                                                  
     choice = None
 
+    LayersSizesList = ""
     ModelTypeStr = ""
     ActivationLayersList = ""
     LayerTypesList = ""
@@ -95,31 +98,40 @@ def WinWorkerDialog():
             ModelTypeStr = values[event]
             ModelType = ModelTypeMapping[ModelTypeStr]
             print(f"Model Id: {ModelType} {ModelTypeStr}")
+        
+
+        # Layers Sizes List
+        if event == "LAYERS-SIZES-INPUT":
+            LayersSizesList = values[event]
+            WorkerWindow["-LAYERS-SIZES-INPUT-"].update(f'({str(count_str_list_elements(LayersSizesList))})')
 
         #TODO layers sizes
+
+         # Layers Types output list handling:
+        selection_key = '-LAYER-TYPE-SELECTION-'
+        codes_key = '-LAYER-TYPE-CODES-INPUT-'
+        add_butt_key = '-LAYER-TYPE-SELECTION-ADD-'
+        clear_butt_key = '-LAYER-TYPE-SELECTION-CLEAR-'
+        LayerTypesList = combo_list_editable_handler(WorkerWindow, event, values, LayerTypeMap, LayerTypesList,
+                                                    selection_key, codes_key, add_butt_key, clear_butt_key)
+        WorkerWindow["-NUM-OF-LAYERS-TYPES-"].update(f'({str(count_str_list_elements(LayerTypesList))})')
+
+        if event == "-LAYER-TYPE-HELP-":
+            sg.popup_ok(f"Layer type codes:\n{pretty_print_dict(LayerTypeMap)}", keep_on_top=True, title="Layer Type Codes")
+
 
         # Activation codes combo and output list handling:
         selection_key = '-ACTIVATION-LAYER-SELECTION-'
         codes_key = '-ACTIVATION-CODES-INPUT-'
         add_butt_key = '-ACTIVATION-LAYER-SELECTION-ADD-'
         clear_butt_key = '-ACTIVATION-LAYER-SELECTION-CLEAR-'
-        combo_list_editable_handler(WorkerWindow, event, values, ActivationFunctionsMap, ActivationLayersList,
-                                    selection_key, codes_key, add_butt_key, clear_butt_key)
+        ActivationLayersList = combo_list_editable_handler(WorkerWindow, event, values, ActivationFunctionsMap, ActivationLayersList,
+                                                            selection_key, codes_key, add_butt_key, clear_butt_key)
         WorkerWindow["-NUM-OF-LAYERS-ACTIVATIONS-"].update(f'({str(count_str_list_elements(ActivationLayersList))})')
         if event == "-LAYER-TYPE-HELP-":
             sg.popup_ok(f"Layer type codes:\n{pretty_print_dict(LayerTypeMap)}", keep_on_top=True, title="Layer Type Codes")
 
-        # Activation codes combo and output list handling:
-        selection_key = '-LAYER-TYPE-SELECTION-'
-        codes_key = '-LAYER-TYPE-CODES-INPUT-'
-        add_butt_key = '-LAYER-TYPE-SELECTION-ADD-'
-        clear_butt_key = '-LAYER-TYPE-SELECTION-CLEAR-'
-        combo_list_editable_handler(WorkerWindow, event, values, LayerTypeMap, LayerTypesList,
-                                    selection_key, codes_key, add_butt_key, clear_butt_key)
-        if event == "-LAYER-TYPE-HELP-":
-            sg.popup_ok(f"Layer type codes:\n{pretty_print_dict(LayerTypeMap)}", keep_on_top=True, title="Layer Type Codes")
-        WorkerWindow["-NUM-OF-LAYERS-TYPES-"].update(f'({str(count_str_list_elements(LayerTypesList))})')
-
+       
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
 
