930216dcf155881605fe3f0bf6d1f53dc7932486
leondavi <ddleon@outlook.co.il>
2023-06-20 00:21:57 +0300

    [NIF] Add destroy - not checked!
    First part of implementation of get/set weights test

--- src_erl/erlBridge/nerlNIF.erl
+++ src_erl/erlBridge/nerlNIF.erl
@@ -3,7 +3,7 @@
 -include("nerlTensor.hrl").
 
 -export([init/0,nif_preload/0,create_nif/6, destroy_nif/1, train_nif/6,call_to_train/6,predict_nif/3,call_to_predict/6,get_weights_nif/1,printTensor/2]).
--export([call_to_get_weights/2,call_to_set_weights/2]).
+-export([call_to_get_weights/1,call_to_set_weights/2]).
 -export([decode_nif/2, nerltensor_binary_decode/2]).
 -export([encode_nif/2, nerltensor_encode/5, nerltensor_conversion/2, get_all_binary_types/0, get_all_nerltensor_list_types/0]).
 -export([erl_type_conversion/1]).
