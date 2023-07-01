7922f7f9eea37771ac87971ff185550e0e97ea54
leondavi <ddleon@outlook.co.il>
2023-07-01 20:45:21 +0300

    Add NerlPlanner bash script

---
+++ NerlnetPlanner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# TODO install and validate prerequisites
+NERLNET_DIR="/usr/local/lib/nerlnet-lib/NErlNet"
+NERLPLANNER_DIR=$NERLNET_DIR/src_py/nerlPlanner
+NERLPLANNER_MAIN_PY=$NERLPLANNER_DIR/main.py
+
+python3 $NERLPLANNER_MAIN_PY
