28d14abc6b831483bb4774db5c485fe89db9a244
leondavi <ddleon@outlook.co.il>
2023-07-04 21:58:22 +0300

    [JSONGEN] WIP Implement settings block

--- src_py/nerlPlanner/Handlers.py
+++ src_py/nerlPlanner/Handlers.py
@@ -1,20 +1,30 @@
 from JsonElements import *
 from JsonArchitecture import JsonArchitecture
+from Definitions import *
 
 # instances and lists of instances
 json_architecture_instance = JsonArchitecture()
 
 # instances
-frequency = None
 batch_size = None
 main_server_inst = None
 api_server_inst = None
 nerl_gui_inst = None
 
-
-
 def reset_instances():
     json_architecture_instance = JsonArchitecture()
+
+def settings_handler(event, values):
+    frequency = None
+    frequency_inst = None
+    if event == KEY_SETTINGS_ADD_BUTTON:
+        frequency = values[KEY_SETTINGS_FREQUENCY_INPUT] if values[KEY_SETTINGS_FREQUENCY_INPUT] else None
+        frequency_inst = Frequency(frequency) if frequency else None
+        print(f"Frequency={frequency}") #TODO remove - jsut debug
+
+        batch_size = values[KEY_SETTINGS_BATCH_SIZE_INPUT] if values[KEY_SETTINGS_BATCH_SIZE_INPUT] else None
+        batch_size_inst = BatchSize(batch_size) if batch_size else None
+        print(f"batch_size={batch_size}") #TODO remove - jsut debug
 
 def update_current_json_file_path(jsonPath):
     print(jsonPath)
