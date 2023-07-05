780c04b962ab5738a6eed0675a544a645a9c8873
leondavi <ddleon@outlook.co.il>
2023-07-06 02:05:50 +0300

    [JSONGEN] WIP Settings and Special entities creation

--- src_py/nerlPlanner/Handlers.py
+++ src_py/nerlPlanner/Handlers.py
@@ -31,23 +31,31 @@ def settings_handler(event, values):
 
         main_server_ip = values[KEY_SETTINGS_MAINSERVER_IP_INPUT] if values[KEY_SETTINGS_MAINSERVER_IP_INPUT] else None
         main_server_port = values[KEY_SETTINGS_MAINSERVER_PORT_INPUT] if values[KEY_SETTINGS_MAINSERVER_PORT_INPUT] else None
-        main_server_inst = MainServer(main_server_ip, main_server_port)
+        main_server_args = values[KEY_SETTINGS_MAINSERVER_ARGS_INPUT] if values[KEY_SETTINGS_MAINSERVER_ARGS_INPUT] else None
+        main_server_inst = MainServer(main_server_ip, main_server_port, main_server_args) if main_server_ip and main_server_port else None
         error_list.append(main_server_inst.error())
 
         api_server_ip = values[KEY_SETTINGS_APISERVER_IP_INPUT] if values[KEY_SETTINGS_APISERVER_IP_INPUT] else None
         api_server_port = values[KEY_SETTINGS_APISERVER_PORT_INPUT] if values[KEY_SETTINGS_APISERVER_PORT_INPUT] else None
-        api_server_inst = MainServer(api_server_ip, api_server_port)
+        api_server_args = values[KEY_SETTINGS_APISERVER_ARGS_INPUT] if values[KEY_SETTINGS_APISERVER_ARGS_INPUT] else None
+        api_server_inst = ApiServer(api_server_ip, api_server_port, api_server_args) if api_server_ip and api_server_port else None
         error_list.append(api_server_inst.error())
 
-        if values[KEY_SETTINGS_NERLGUI_IP_INPUT] and values[KEY_SETTINGS_NERLGUI_PORT_INPUT]:
+        nerlgui_server_inst = None
+        if values[KEY_SETTINGS_NERLGUI_IP_INPUT] and values[KEY_SETTINGS_NERLGUI_PORT_INPUT] and values[KEY_CHECKBOX_ENABLE_NERLGUI]:
             nerlgui_server_ip = values[KEY_SETTINGS_NERLGUI_IP_INPUT] if values[KEY_SETTINGS_NERLGUI_IP_INPUT] else None
             nerlgui_server_port = values[KEY_SETTINGS_NERLGUI_PORT_INPUT] if values[KEY_SETTINGS_NERLGUI_PORT_INPUT] else None
-            nerlgui_server_inst = MainServer(nerlgui_server_ip, nerlgui_server_port)
+            nerlgui_server_args = values[KEY_SETTINGS_NERLGUI_ARGS_INPUT] if values[KEY_SETTINGS_NERLGUI_ARGS_INPUT] else None
+            nerlgui_server_inst = NerlGUI(nerlgui_server_ip, nerlgui_server_port, nerlgui_server_args)
             error_list.append(nerlgui_server_inst.error())
         
         error = any(error_list)
         if error:
             pass # pop up windows with issues
+
+        json_architecture_instance.add_nerlnet_settings(frequency_inst, batch_size_inst)
+        json_architecture_instance.add_main_server(main_server_inst)
+        json_architecture_instance.add_api_server(api_server_inst)
 
 
 
