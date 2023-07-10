ce30f17d9e95498e9ce0b6ea076de1f6ecba4070
halfway258 <harancoh@gmail.com>
2023-07-10 18:27:54 +0300

    missed message stats, source split mode

--- src_erl/Communication_Layer/http_Nerlserver/src/init/jsonParser.erl
+++ src_erl/Communication_Layer/http_Nerlserver/src/init/jsonParser.erl
@@ -10,7 +10,7 @@
 -include("../nerl_tools.hrl").
 -export([getHostEntities/3, json_to_ets/2]).
 
--define(SINGLE_STRONG_COMPONENT,1).
+-define(SINGLE_STRONG_COMPONENT,2). %% apiServer alone + all network 
 -define(ETS_DATA_IDX, 2).
 -define(PORT_IDX, 1). % port is always at the first index of any entity that has a port!
 -define(NERLNET_DATA_ETS_LOG_DST, "/usr/local/lib/nerlnet-lib/log/nerlnet_data_ets.log").
@@ -170,13 +170,13 @@ getHostEntities(ArchitectureMap,CommunicationMap, HostNameBin)->
 
   NerlnetGraph = buildCommunicationGraph(ArchitectureMap, CommunicationMap),
 
-  Sources = ets:lookup_element(nerlnet_data, sources, ?ETS_DATA_IDX),
-  VerticesNames = digraph:vertices(NerlnetGraph),
-  SourcesInGraph = [S || S <- maps:keys(Sources), lists:member(S, VerticesNames)],
+  % Sources = ets:lookup_element(nerlnet_data, sources, ?ETS_DATA_IDX),
+  % VerticesNames = digraph:vertices(NerlnetGraph),
+  % SourcesInGraph = [S || S <- maps:keys(Sources), lists:member(S, VerticesNames)],
 
-  AllCreated = length(maps:keys(Sources)) == length(SourcesInGraph),
-  if AllCreated -> cont;
-  true -> throw("not all sources connected!!") end,
+  % AllCreated = length(maps:keys(Sources)) == length(SourcesInGraph),
+  % if AllCreated -> cont;
+  % true -> throw("not all sources connected!!") end,
 
   % add graph to ets
   % save log of data extracted from json
@@ -241,12 +241,12 @@ connectRouters(Graph,_ArchitectureMap,CommunicationMap) ->
     ?LOG_INFO("RoutersEdges:~p~n",[RoutersEdges]),
     AddEdgesFunc = fun({Router,Entity}) -> add_edges(Graph,Router,Entity)  end,
     lists:foreach(AddEdgesFunc, RoutersEdges),
-    StrongComponents = digraph_utils:strong_components(Graph).
-    % ?LOG_INFO("Graph update - strong components: ~p",[StrongComponents]),
-    % case length(StrongComponents) of
-    %   ?SINGLE_STRONG_COMPONENT -> cont;
-    %   _ -> throw("not all entities connected! Change connection map!!")
-    % end.
+    StrongComponents = digraph_utils:strong_components(Graph),
+    ?LOG_INFO("Graph update - strong components: ~p",[StrongComponents]),
+    case length(StrongComponents) of
+      ?SINGLE_STRONG_COMPONENT -> cont;
+      _ -> throw("not all entities connected! Change connection map!!")
+    end.
 
 % returns true if edge appears in graph or false if not
 edge_in_graph(Graph, Vertex1, Vertex2) when is_atom(Vertex1) and is_atom(Vertex2) ->  
