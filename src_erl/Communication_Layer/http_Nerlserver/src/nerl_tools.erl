ecba24f04b617c5b106b53cd17bcd03a5d672667
halfway258 <harancoh@gmail.com>
2023-07-12 18:41:24 +0300

    binary fix, experiments

--- src_erl/Communication_Layer/http_Nerlserver/src/nerl_tools.erl
+++ src_erl/Communication_Layer/http_Nerlserver/src/nerl_tools.erl
@@ -97,10 +97,16 @@ read_all_parts(Req0, Got) ->
   %% gets multipart data and combines it
 
 read_all_data(Req0, Got) ->
-  %io:format("length of read data so far: ~p~n",[length(Got)]),
+  % io:format("length of read data so far: ~p~n",[length(Got)]),
   case cowboy_req:read_body(Req0) of
-      {more, Data, Req} -> read_all_data(Req, Got++Data);
-      {ok, Data, Req} -> {Req, Got++Data}
+      {more, Data, Req} -> 
+        if 
+          Got == <<>>     ->  read_all_data(Req, <<Data/binary>>);
+          is_binary(Data) ->  read_all_data(Req, <<Got/binary, Data/binary>>);
+          true            ->  read_all_data(Req, Got++Data) end;
+      {ok, Data, Req}   -> 
+        if is_binary(Data) -> {Req, <<Got/binary, Data/binary>>};
+        true ->               {Req, Got++Data} end
   end.
 
 deleteOldJson(FilePath) ->
