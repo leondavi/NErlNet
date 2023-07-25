89882c2140bd0b25676b229f9990e29ab06b1e8a
halfway258 <harancoh@gmail.com>
2023-07-25 20:59:31 +0300

    5 device test

--- NerlnetGetData.sh
+++ NerlnetGetData.sh
@@ -5,7 +5,13 @@ NERLNET_PREFIX="[GET_DATA]"
 INPUT_DATA_DIR="inputDataDir"
 
 if [ -d "$INPUT_DATA_DIR" ]; then
-        echo "$NERLNET_PREFIX Input data directory of nerlnet exists at: /$INPUT_DATA_DIR"
+        echo "$NERLNET_PREFIX deleting old input data directory at /$INPUT_DATA_DIR"
+        rm -rf /$INPUT_DATA_DIR/
+        echo "$NERLNET_PREFIX /$INPUT_DATA_DIR is being generated and filled with default data"
+        wget https://github.com/halfway258/NerlnetData/archive/refs/heads/main.zip
+        unzip main.zip -d $INPUT_DATA_DIR
+        rm main.zip
+        echo "$NERLNET_PREFIX Add input data to $INPUT_DATA_DIR"
 else
         echo "$NERLNET_PREFIX /$INPUT_DATA_DIR is being generated and filled with default data"
         wget https://github.com/halfway258/NerlnetData/archive/refs/heads/main.zip
