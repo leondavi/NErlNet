96d59f5b1f91f0b644f74f40baa3f4dc3cc5f7a4
leondavi <ddleon@outlook.com>
2023-07-31 00:13:48 +0300

    [JSONGEN] Add installation packages validation

--- NerlnetPlanner.sh
+++ NerlnetPlanner.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-# TODO install and validate prerequisites
+print()
+{
+    echo "[NERLPLANNER] $1"
+}
+
 NERLNET_DIR="/usr/local/lib/nerlnet-lib/NErlNet"
 NERLPLANNER_DIR=$NERLNET_DIR/src_py/nerlPlanner
 NERLPLANNER_MAIN_PY=$NERLPLANNER_DIR/main.py
+
+
+# install and validate prerequisites
+
+DOT_PACKAGE="python3-pydot"
+DOT_EXIST=$(dpkg-query -W --showformat='${Status}\n' $DOT_PACKAGE | grep "install ok installed")
+print "Checking for $DOT_PACKAGE"
+if [ "" = "$DOT_EXIST" ]; then
+  print "Please run: sudo apt-get install $DOT_PACKAGE"
+else
+  print "installed"
+fi
+
+
+GRAPHVIZ_PACKAGE="graphviz"
+GRAPHVIZ_EXIST=$(dpkg-query -W --showformat='${Status}\n' $GRAPHVIZ_PACKAGE|grep "install ok installed")
+print "Checking for $GRAPHVIZ_PACKAGE"
+if [ "" = "$GRAPHVIZ_EXIST" ]; then
+  print "Please run: sudo apt-get install $GRAPHVIZ_PACKAGE"
+else
+  print "installed"
+fi
 
 python3 $NERLPLANNER_MAIN_PY
