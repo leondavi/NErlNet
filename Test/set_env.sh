45a2f2c1373b5d7fc1e5fe374369e187ea19b689
leondavi <ddleon@outlook.co.il>
2023-09-05 19:29:31 +0300

    [TEST] venv set env

--- Test/set_env.sh
+++ Test/set_env.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+NERLNET_VENV_PATH="/tmp/nerlnet/venv"
+
 function print()
 {
     echo "[NERLNET-TEST][SET-ENV] $1"
 }
 # set python environment to run Nerlnet Flow
 print("Starts")
+
+python3 -m $NERLNET_VENV_PATH
+
+cat src_py/requirements.txt
+
+source $NERLNET_VENV_PATH/bin/activate
+pip3 install -r src_py/requirements.txt
