f7d412c398a7764f61fb2f1d8f891d3d3a250fb8
leondavi <ddleon@outlook.co.il>
2023-09-12 04:25:50 +0300

    [NERLNET_RUN] Refactoring - Fix CI issue

--- src_py/apiServer/experiment_flow_test.py
+++ src_py/apiServer/experiment_flow_test.py
@@ -10,7 +10,8 @@ def print_test(in_str : str):
 
 NERLNET_PATH = os.getenv('NERLNET_PATH')
 TESTS_PATH = os.getenv('TESTS_PATH')
-NERLNET_RUN_SCRIPT = "./NerlnetRun.sh"
+NERLNET_RUN_SCRIPT = "./NerlnetRun.sh --run-mode release"
+NERLNET_RUN_STOP_SCRIPT = "./NerlnetRun.sh --run-mode stop"
 NERLNET_RUNNING_TIMEOUT_SEC = int(os.getenv('NERLNET_RUNNING_TIMEOUT_SEC'))
 
 # TODO JUST FOR DEBUG
@@ -30,18 +31,25 @@ api_server_instance.setJsons(0,0,0)
 
 arch_json , connmap_json, exp_flow_json = api_server_instance.getUserJsons()
 
-# To see CI issue open this code:
+api_server_instance.initialization(arch_json , connmap_json, exp_flow_json)
+api_server_instance.sendJsonsToDevices()
+
+
+nerlnet_stop_cmd = RunCommand(NERLNET_RUN_STOP_SCRIPT, NERLNET_PATH)
 stdout, stderr, rc = nerlnet_run_cmd.sync(NERLNET_RUNNING_TIMEOUT_SEC)
 print_test(rc)
 if stderr: 
     print_test(stderr)
 else:
     print_test(stdout)
+stdout, stderr, rc = nerlnet_stop_cmd.sync(NERLNET_RUNNING_TIMEOUT_SEC)
+print_test(rc)
+if stderr: 
+    print_test(stderr)
+else:
+    print_test(stdout)
+
 raise "break exception"
-
-api_server_instance.initialization(arch_json , connmap_json, exp_flow_json)
-api_server_instance.sendJsonsToDevices()
-
 # Lines checked locally
 # stdout, stderr, rc = nerlnet_run_cmd.sync(NERLNET_RUNNING_TIMEOUT_SEC)
 # print_test(rc)
