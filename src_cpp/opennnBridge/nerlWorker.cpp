b2a1aaf33c433d7991281ad1997c17690cf8670a
OriSadek <ori.sadek@gmail.com>
2023-11-30 19:45:34 +0200

    adding cases to switches new

--- src_cpp/opennnBridge/nerlWorker.cpp
+++ src_cpp/opennnBridge/nerlWorker.cpp
@@ -54,14 +54,14 @@ namespace nerlnet
         int res;
         switch (layer_type)
         {
-            case LAYER_TYPE_DEFAULT:{ res = (int)opennn::Layer::Type::Perceptron; break;}
-            case LAYER_TYPE_SCALING:{ res = (int)opennn::Layer::Type::Scaling; break;}
-            case LAYER_TYPE_CNN: {res = (int)opennn::Layer::Type::Convolutional; break;}
-            case LAYER_TYPE_PERCEPTRON: {res = (int)opennn::Layer::Type::Perceptron; break;}
-            case LAYER_TYPE_POOLING:{ res = (int)opennn::Layer::Type::Pooling; break;}
-            case LAYER_TYPE_PROBABILISTIC:{ res = (int)opennn::Layer::Type::Probabilistic; break;}
-            case LAYER_TYPE_LSTM:{ res = (int)opennn::Layer::Type::LongShortTermMemory; break;}
-            case LAYER_TYPE_RECCURRENT: {res = (int)opennn::Layer::Type::Recurrent; break;}
+            case LAYER_TYPE_DEFAULT:      { res = (int)opennn::Layer::Type::Perceptron;          break;}
+            case LAYER_TYPE_SCALING:      { res = (int)opennn::Layer::Type::Scaling;             break;}
+            case LAYER_TYPE_CNN:          { res = (int)opennn::Layer::Type::Convolutional;       break;}
+            case LAYER_TYPE_PERCEPTRON:   { res = (int)opennn::Layer::Type::Perceptron;          break;}
+            case LAYER_TYPE_POOLING:      { res = (int)opennn::Layer::Type::Pooling;             break;}
+            case LAYER_TYPE_PROBABILISTIC:{ res = (int)opennn::Layer::Type::Probabilistic;       break;}
+            case LAYER_TYPE_LSTM:         { res = (int)opennn::Layer::Type::LongShortTermMemory; break;}
+            case LAYER_TYPE_RECCURRENT:   { res = (int)opennn::Layer::Type::Recurrent;           break;}
         }
        return res;
     }
@@ -73,20 +73,20 @@ namespace nerlnet
 
     int translate_activation_function(int activation_function)
     {
-      int res;
+       int res;
        switch (activation_function)
        {
-       case ACTIVATION_THRESHOLD: {res = (int)opennn::PerceptronLayer::ActivationFunction::Threshold; break;}
-       case ACTIVATION_SIGN: {res = (int)opennn::PerceptronLayer::ActivationFunction::SymmetricThreshold;   break;}
-       case ACTIVATION_LOGISTIC:{res = (int)opennn::PerceptronLayer::ActivationFunction::Logistic;    break;}
-       case ACTIVATION_TANH: {res = (int)opennn::PerceptronLayer::ActivationFunction::HyperbolicTangent; break;}
-       case ACTIVATION_LINEAR: {res = (int)opennn::PerceptronLayer::ActivationFunction::Linear;      break;}
-       case ACTIVATION_RELU: {res = (int)opennn::PerceptronLayer::ActivationFunction::RectifiedLinear;    break;}
-       case ACTIVATION_ELU: {res = (int)opennn::PerceptronLayer::ActivationFunction::ExponentialLinear;    break;}
-       case ACTIVATION_SELU: {res = (int)opennn::PerceptronLayer::ActivationFunction::ScaledExponentialLinear;  break;}
-       case ACTIVATION_SOFT_PLUS: {res = (int)opennn::PerceptronLayer::ActivationFunction::SoftPlus;   break;}
-       case ACTIVATION_SOFT_SIGN: {res = (int)opennn::PerceptronLayer::ActivationFunction::SoftSign ;  break;}
-       case ACTIVATION_HARD_SIGMOID: {res = (int)opennn::PerceptronLayer::ActivationFunction::HardSigmoid; break;}
+       case ACTIVATION_THRESHOLD:    { res = (int)opennn::PerceptronLayer::ActivationFunction::Threshold;                break;}
+       case ACTIVATION_SIGN:         { res = (int)opennn::PerceptronLayer::ActivationFunction::SymmetricThreshold;       break;}
+       case ACTIVATION_LOGISTIC:     { res = (int)opennn::PerceptronLayer::ActivationFunction::Logistic;                 break;}
+       case ACTIVATION_TANH:         { res = (int)opennn::PerceptronLayer::ActivationFunction::HyperbolicTangent;        break;}
+       case ACTIVATION_LINEAR:       { res = (int)opennn::PerceptronLayer::ActivationFunction::Linear;                   break;}
+       case ACTIVATION_RELU:         { res = (int)opennn::PerceptronLayer::ActivationFunction::RectifiedLinear;          break;}
+       case ACTIVATION_ELU:          { res = (int)opennn::PerceptronLayer::ActivationFunction::ExponentialLinear;        break;}
+       case ACTIVATION_SELU:         { res = (int)opennn::PerceptronLayer::ActivationFunction::ScaledExponentialLinear;  break;}
+       case ACTIVATION_SOFT_PLUS:    { res = (int)opennn::PerceptronLayer::ActivationFunction::SoftPlus;                 break;}
+       case ACTIVATION_SOFT_SIGN:    { res = (int)opennn::PerceptronLayer::ActivationFunction::SoftSign;                 break;}
+       case ACTIVATION_HARD_SIGMOID: { res = (int)opennn::PerceptronLayer::ActivationFunction::HardSigmoid;              break;}
        }
         return res;
     }
@@ -96,28 +96,28 @@ namespace nerlnet
         int res;
         switch (loss_method)
         {
-        case LOSS_METHOD_SSE:{res = (int)opennn::TrainingStrategy::LossMethod::SUM_SQUARED_ERROR;  break;}
-        case LOSS_METHOD_MSE:{res = (int)opennn::TrainingStrategy::LossMethod::MEAN_SQUARED_ERROR; break;}
-        case LOSS_METHOD_NSE:{res = (int)opennn::TrainingStrategy::LossMethod::NORMALIZED_SQUARED_ERROR; break;}
-        case LOSS_METHOD_MINKOWSKIE:{res = (int)opennn::TrainingStrategy::LossMethod::MINKOWSKI_ERROR;break;}
-        case LOSS_METHOD_WSE:{res = (int)opennn::TrainingStrategy::LossMethod::WEIGHTED_SQUARED_ERROR;break;}
-        case LOSS_METHOD_CEE:{res = (int)opennn::TrainingStrategy::LossMethod::CROSS_ENTROPY_ERROR;break;}
+        case LOSS_METHOD_SSE:        { res = (int)opennn::TrainingStrategy::LossMethod::SUM_SQUARED_ERROR;        break;}
+        case LOSS_METHOD_MSE:        { res = (int)opennn::TrainingStrategy::LossMethod::MEAN_SQUARED_ERROR;       break;}
+        case LOSS_METHOD_NSE:        { res = (int)opennn::TrainingStrategy::LossMethod::NORMALIZED_SQUARED_ERROR; break;}
+        case LOSS_METHOD_MINKOWSKIE: { res = (int)opennn::TrainingStrategy::LossMethod::MINKOWSKI_ERROR;          break;}
+        case LOSS_METHOD_WSE:        { res = (int)opennn::TrainingStrategy::LossMethod::WEIGHTED_SQUARED_ERROR;   break;}
+        case LOSS_METHOD_CEE:        { res = (int)opennn::TrainingStrategy::LossMethod::CROSS_ENTROPY_ERROR;      break;}
         }
         return res;
     }
     int translate_optimizer_type(int optimizer_type)
-    {
+    {   
+        int res;
         switch (optimizer_type)
-        { ///check later
-        case OPTIMIZER_NONE:break;
-        case OPTIMIZER_SGD:opennn::TrainingStrategy::OptimizationMethod::STOCHASTIC_GRADIENT_DESCENT; break;
-        case OPTIMIZER_MINI_BATCH: opennn::TrainingStrategy::OptimizationMethod::GRADIENT_DESCENT;
-        case OPTIMIZER_MOMENTUM: opennn::TrainingStrategy::OptimizationMethod::ADAPTIVE_MOMENT_ESTIMATION; break;
-        case OPTIMIZER_NAG: opennn::TrainingStrategy::OptimizationMethod::QUASI_NEWTON_METHOD; break;
-        case OPTIMIZER_ADAGRAD:opennn::TrainingStrategy::OptimizationMethod::LEVENBERG_MARQUARDT_ALGORITHM; break;
-        case OPTIMIZER_ADAM: opennn::TrainingStrategy::OptimizationMethod::ADAPTIVE_MOMENT_ESTIMATION; break;
+        { 
+        case OPTIMIZER_GD:          { res = (int)opennn::TrainingStrategy::OptimizationMethod::GRADIENT_DESCENT;              break;}
+        case OPTIMIZER_SGD:         { res = (int)opennn::TrainingStrategy::OptimizationMethod::STOCHASTIC_GRADIENT_DESCENT;   break;}
+        case OPTIMIZER_CGD:         { res = (int)opennn::TrainingStrategy::OptimizationMethod::GRADIENT_DESCENT;              break;}
+        case OPTIMIZER_QUASINEUTON: { res = (int) opennn::TrainingStrategy::OptimizationMethod::QUASI_NEWTON_METHOD;          break;}
+        case OPTIMIZER_LVM:         { res = (int)opennn::TrainingStrategy::OptimizationMethod::LEVENBERG_MARQUARDT_ALGORITHM; break;}
+        case OPTIMIZER_ADAM:        { res = (int)opennn::TrainingStrategy::OptimizationMethod::ADAPTIVE_MOMENT_ESTIMATION;    break;}
         }
-        return 0;
+        return res;
     }
 
     int translate_scaling_method(int scaling_method)
@@ -144,8 +144,8 @@ namespace nerlnet
         case MODEL_TYPE_NN: break;
         case MODEL_TYPE_AUTOENCODER: break;
         case MODEL_TYPE_AE_CLASSIFIER: break;
-        case MODEL_TYPE_FED_CLIENT: break;
-        case MODEL_TYPE_FED_SERVER: break;
+      //  case MODEL_TYPE_FED_CLIENT: break;
+       // case MODEL_TYPE_FED_SERVER: break;
         }
         return 0;
     }
