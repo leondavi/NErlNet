8f0c4e7e2b4afc39d3d102df4cabd612ae2756ab
OriSadek <ori.sadek@gmail.com>
2023-12-31 17:29:36 +0000

    add activation functions and continue to build the nn

--- src_cpp/opennnBridge/nerlWorkerOpenNN.cpp
+++ src_cpp/opennnBridge/nerlWorkerOpenNN.cpp
@@ -118,23 +118,56 @@ namespace nerlnet
                 {
                     if (curr_layer->is_last())
                     {
-                       // TODO Ori and Nadav - throw exception
+                       throw std::invalid_argument("NerlWorkerOpenNN::generate_custom_model_nn - PERCEPTRON cannot be last layer");
                     }
                     std::shared_ptr<NerlLayer> next_layer = curr_layer->get_next_layer_ptr();
                     int next_layer_size = next_layer->get_dim_size(DIM_X_IDX);
                     int layer_size_curr = curr_layer->get_dim_size(DIM_X_IDX);
+                    int get_layer_functionality = curr_layer->get_layer_functionality();
                     PerceptronLayer* newLayer =  new opennn::PerceptronLayer(layer_size_curr, next_layer_size);
+                    newLayer->set_activation_function(translate_activation_function(get_layer_functionality));
                     neural_network_ptr->add_layer(newLayer);
+                    break;
                 }
                 case LAYER_TYPE_SCALING:
                 {
                     std::shared_ptr<NerlLayer> next_layer = curr_layer->get_next_layer_ptr();
                     std::vector<int> layer_dims_vec;
                     curr_layer->get_layer_size(layer_dims_vec);
-                    //iTensor1D layer_size_curr = iTensor1D(layer_dims_vec.data()); // convert std::vector to tensor 1D
+                    int layer_size_curr = curr_layer->get_dim_size(DIM_X_IDX);
+                    int get_layer_functionality = curr_layer->get_layer_functionality();
+                    ScalingLayer* newLayer = new opennn::ScalingLayer(layer_size_curr);
+                    newLayer->set_scalers(translate_scaling_method(get_layer_functionality));
+                    neural_network_ptr->add_layer(newLayer);
+                    break;
+
                 }
-                // TODO UNSCALING
-                // TODO Probabilistic
+                  case LAYER_TYPE_UNSCALING:
+                {
+                    std::shared_ptr<NerlLayer> next_layer = curr_layer->get_next_layer_ptr();
+                    std::vector<int> layer_dims_vec;
+                    curr_layer->get_layer_size(layer_dims_vec);
+                    int layer_size_curr = curr_layer->get_dim_size(DIM_X_IDX);
+                    int get_layer_functionality = curr_layer->get_layer_functionality();
+                    UnscalingLayer* newLayer = new opennn::UnscalingLayer(layer_size_curr);
+                    newLayer->set_scalers(translate_unscaling_method(get_layer_functionality));
+                    neural_network_ptr->add_layer(newLayer);
+                    break;
+
+                }
+                  case LAYER_TYPE_PROBABILISTIC:
+                {
+                    std::shared_ptr<NerlLayer> next_layer = curr_layer->get_next_layer_ptr();
+                    std::vector<int> layer_dims_vec;
+                    curr_layer->get_layer_size(layer_dims_vec);
+                    int next_layer_size = next_layer->get_dim_size(DIM_X_IDX);
+                    int layer_size_curr = curr_layer->get_dim_size(DIM_X_IDX);
+                    int get_layer_functionality = curr_layer->get_layer_functionality();
+                    ProbabilisticLayer* newLayer =  new opennn::ProbabilisticLayer(layer_size_curr, next_layer_size);
+                    newLayer->set_activation_function((opennn::ProbabilisticLayer::ActivationFunction)translate_activation_function(get_layer_functionality));
+                    neural_network_ptr->add_layer(newLayer);
+                    break;
+                }
             }  
             
             curr_layer = curr_layer->get_next_layer_ptr();
@@ -166,15 +199,15 @@ namespace nerlnet
         int res;
         switch (layer_type)
         {
-            case LAYER_TYPE_DEFAULT:      { res = translate_activation_function(layer_functionality); break;} // PERCEPTRON
-            case LAYER_TYPE_SCALING:      { res = translate_scaling_method(layer_functionality);      break;}
-            case LAYER_TYPE_CNN:          { res = translate_activation_function(layer_functionality); break;}
-            case LAYER_TYPE_PERCEPTRON:   { res = translate_activation_function(layer_functionality); break;}
-            case LAYER_TYPE_POOLING:      { res = translate_pooling_method(layer_functionality);      break;}
-            case LAYER_TYPE_PROBABILISTIC:{ res = translate_activation_function(layer_functionality); break;}
-            case LAYER_TYPE_LSTM:         { res = translate_activation_function(layer_functionality); break;}
-            case LAYER_TYPE_RECCURRENT:   { res = translate_activation_function(layer_functionality); break;}
-            case LAYER_TYPE_UNSCALING:    { res = translate_unscaling_method(layer_functionality);    break;}
+            case LAYER_TYPE_DEFAULT:      { res = translate_activation_function_int(layer_functionality); break;} // PERCEPTRON
+            case LAYER_TYPE_SCALING:      { res = translate_scaling_method_int(layer_functionality);      break;}
+            case LAYER_TYPE_CNN:          { res = translate_activation_function_int(layer_functionality); break;}
+            case LAYER_TYPE_PERCEPTRON:   { res = translate_activation_function_int(layer_functionality); break;}
+            case LAYER_TYPE_POOLING:      { res = translate_pooling_method_int(layer_functionality);      break;}
+            case LAYER_TYPE_PROBABILISTIC:{ res = translate_activation_function_int(layer_functionality); break;}
+            case LAYER_TYPE_LSTM:         { res = translate_activation_function_int(layer_functionality); break;}
+            case LAYER_TYPE_RECCURRENT:   { res = translate_activation_function_int(layer_functionality); break;}
+            case LAYER_TYPE_UNSCALING:    { res = translate_unscaling_method_int(layer_functionality);    break;}
         }
        return res;
     }
@@ -196,7 +229,7 @@ namespace nerlnet
        return res;
     }
 
-    int NerlWorkerOpenNN::translate_activation_function(int activation_function)
+    int NerlWorkerOpenNN::translate_activation_function_int(int activation_function)
     {
        int res;
        switch (activation_function)
@@ -212,6 +245,26 @@ namespace nerlnet
        case ACTIVATION_SOFT_PLUS:    { res = (int)opennn::PerceptronLayer::ActivationFunction::SoftPlus;                 break;}
        case ACTIVATION_SOFT_SIGN:    { res = (int)opennn::PerceptronLayer::ActivationFunction::SoftSign;                 break;}
        case ACTIVATION_HARD_SIGMOID: { res = (int)opennn::PerceptronLayer::ActivationFunction::HardSigmoid;              break;}
+       }
+       return res;
+    }
+
+  PerceptronLayer::ActivationFunction NerlWorkerOpenNN::translate_activation_function(int activation_function)
+    {
+    PerceptronLayer::ActivationFunction res;
+       switch (activation_function)
+       {
+       case ACTIVATION_THRESHOLD:    { res = opennn::PerceptronLayer::ActivationFunction::Threshold;                break;}
+       case ACTIVATION_SIGN:         { res = opennn::PerceptronLayer::ActivationFunction::SymmetricThreshold;       break;}
+       case ACTIVATION_LOGISTIC:     { res = opennn::PerceptronLayer::ActivationFunction::Logistic;                 break;}
+       case ACTIVATION_TANH:         { res = opennn::PerceptronLayer::ActivationFunction::HyperbolicTangent;        break;}
+       case ACTIVATION_LINEAR:       { res = opennn::PerceptronLayer::ActivationFunction::Linear;                   break;}
+       case ACTIVATION_RELU:         { res = opennn::PerceptronLayer::ActivationFunction::RectifiedLinear;          break;}
+       case ACTIVATION_ELU:          { res = opennn::PerceptronLayer::ActivationFunction::ExponentialLinear;        break;}
+       case ACTIVATION_SELU:         { res = opennn::PerceptronLayer::ActivationFunction::ScaledExponentialLinear;  break;}
+       case ACTIVATION_SOFT_PLUS:    { res = opennn::PerceptronLayer::ActivationFunction::SoftPlus;                 break;}
+       case ACTIVATION_SOFT_SIGN:    { res = opennn::PerceptronLayer::ActivationFunction::SoftSign;                 break;}
+       case ACTIVATION_HARD_SIGMOID: { res = opennn::PerceptronLayer::ActivationFunction::HardSigmoid;              break;}
        }
        return res;
     }
@@ -245,7 +298,7 @@ namespace nerlnet
         return res;
     }
 
-    int NerlWorkerOpenNN::translate_scaling_method(int scaling_method)
+    int NerlWorkerOpenNN::translate_scaling_method_int(int scaling_method)
     {
         int res;
         switch (scaling_method)
@@ -259,7 +312,22 @@ namespace nerlnet
         return res;
     }
 
-    int NerlWorkerOpenNN::translate_unscaling_method(int unscaling_method) 
+    opennn::Scaler NerlWorkerOpenNN::translate_scaling_method(int scaling_method)
+    {
+        opennn::Scaler res;
+        switch (scaling_method)
+        {
+        case SCALING_NONE:    { res = opennn::Scaler::NoScaling;             break;}
+        case SCALING_MINMAX:  { res = opennn::Scaler::MinimumMaximum;        break;}
+        case SCALING_MEANSTD: { res = opennn::Scaler::MeanStandardDeviation; break;}
+        case SCALING_STD:     { res = opennn::Scaler::StandardDeviation;     break;}
+        case SCALING_LOG:     { res = opennn::Scaler::Logarithm;             break;}
+        }
+        return res;
+    }
+
+
+    int NerlWorkerOpenNN::translate_unscaling_method_int(int unscaling_method)
     {   // openNN uses the Scaler enum for both scaling and unscaling
         int res; 
         switch (unscaling_method)
@@ -273,7 +341,21 @@ namespace nerlnet
         return res;
     }
 
-    int NerlWorkerOpenNN::translate_pooling_method(int pooling_method) 
+    opennn::Scaler NerlWorkerOpenNN::translate_unscaling_method(int unscaling_method)
+    {
+        opennn::Scaler res;
+        switch (unscaling_method)
+        {
+        case UNSCALING_NONE:    { res = opennn::Scaler::NoScaling;             break;}
+        case UNSCALING_MINMAX:  { res = opennn::Scaler::MinimumMaximum;        break;}
+        case UNSCALING_MEANSTD: { res = opennn::Scaler::MeanStandardDeviation; break;}
+        case UNSCALING_STD:     { res = opennn::Scaler::StandardDeviation;     break;}
+        case UNSCALING_LOG:     { res = opennn::Scaler::Logarithm;             break;}
+        }
+        return res;
+    }
+  
+    int NerlWorkerOpenNN::translate_pooling_method_int(int pooling_method)
     {   
         int res; 
         switch (pooling_method)
@@ -281,6 +363,18 @@ namespace nerlnet
         case POOLING_NONE: { res = (int)opennn::PoolingLayer::PoolingMethod::NoPooling;      break;}
         case POOLING_MAX:  { res = (int)opennn::PoolingLayer::PoolingMethod::MaxPooling;     break;}
         case POOLING_AVG:  { res = (int)opennn::PoolingLayer::PoolingMethod::AveragePooling; break;}
+        }
+        return res;
+    }
+
+    opennn::PoolingLayer::PoolingMethod NerlWorkerOpenNN::translate_pooling_method(int pooling_method)
+    {
+        opennn::PoolingLayer::PoolingMethod res;
+        switch (pooling_method)
+        {
+        case POOLING_NONE: { res = opennn::PoolingLayer::PoolingMethod::NoPooling;      break;}
+        case POOLING_MAX:  { res = opennn::PoolingLayer::PoolingMethod::MaxPooling;     break;}
+        case POOLING_AVG:  { res = opennn::PoolingLayer::PoolingMethod::AveragePooling; break;}
         }
         return res;
     }
