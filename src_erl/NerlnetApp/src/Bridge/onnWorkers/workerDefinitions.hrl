01ce456c2c40cb45815ab17e20fabd81d28f6694
GuyPerets106 <guyperets106@gmail.com>
2024-01-07 21:49:00 +0000

    [NerlPlanner Integration] Worker creation updates

--- src_erl/NerlnetApp/src/Bridge/onnWorkers/workerDefinitions.hrl
+++ src_erl/NerlnetApp/src/Bridge/onnWorkers/workerDefinitions.hrl
@@ -2,4 +2,4 @@
 -define(ETS_KEYVAL_VAL_IDX, 2).
 -define(TENSOR_DATA_IDX, 1).
 
--record(workerGeneric_state, {myName , modelID , distributedBehaviorFunc , distributedWorkerData , currentBatchID , nextState , }).
+-record(workerGeneric_state, {myName , modelID , distributedBehaviorFunc , distributedWorkerData , currentBatchID , nextState , lastPhase}).
