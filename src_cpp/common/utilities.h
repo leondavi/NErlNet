032a77c05db33b1b03f340f00b9223139e5e7564
David Leon <ddleon@outlook.co.il>
2024-01-08 20:15:29 +0000

    [NERLNIF] Add is_integer_number to extract simple and complex parsing of layerSizesStr

--- src_cpp/common/utilities.h
+++ src_cpp/common/utilities.h
@@ -14,6 +14,7 @@ namespace nerlnet_utilities
 template<typename T>
 bool shared_ptr_uninitialized(std::shared_ptr<T> &in_ptr) {return !in_ptr;};
 std::vector<std::string> split_strings_by_comma(std::string &str);
+bool is_integer_number(const std::string &input_str);
 std::vector<std::string> matchRegex(std::string &input, std::regex re);
 
 } // namespace nerlutils
