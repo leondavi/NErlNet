756a21f104d9aabca4068b8d7404b960c0ecdec5
leondavi <ddleon@outlook.co.il>
2024-02-09 01:39:18 +0200

    disable communication win dialog

--- src_py/nerlPlanner/WinCommunicationMapDialog.py
+++ src_py/nerlPlanner/WinCommunicationMapDialog.py
@@ -4,7 +4,7 @@ COMMUNICATION_MAP_WINDOW_TITLE = "Generate Communication Map Json File"
 
 def WinCommunicationMapDialog():
     CommunicationMapWindow  = sg.Window(title=COMMUNICATION_MAP_WINDOW_TITLE, layout=[[]],modal=True, keep_on_top=True)                                                  
-    while True:
+    while False: # TODO change to True when available
         event, values = CommunicationMapWindow.read()
 
         if event == "Exit" or event == sg.WIN_CLOSED:
