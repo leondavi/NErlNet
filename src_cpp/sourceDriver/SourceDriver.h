6fdce1e1f1c4cba368a885c9cb39e42221a50b6b
Hugina <nadavhugi@gmail.com>
2024-02-27 17:20:07 +0000

    sourceDriver.h

---
+++ src_cpp/sourceDriver/SourceDriver.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include "common_definitions.h"
+#include "utilities.h"
+
+namespace nerlnet
+{
+
+class SourceDriver
+{
+    public:
+    SourceDriver(int type, int batch_id, int duration, std::vector<std::string> targets, int policy, int batch_size,
+                 double desired_bps, std::string source_name, double supported_bps);
+    ~SourceDriver();             
+    virtual bool isready();
+    virtual int GetBatch();
+
+    protected:
+    int _type;
+    int _batch_id;
+    int _duration;
+    std::vector<std::string> _targets;
+    int _policy;
+    int _batch_size;
+    double _desired_bps;
+    std::string _source_name;
+    double _supported_bps;
+
+    private:
+};
+
+} // namespace nerlnet
