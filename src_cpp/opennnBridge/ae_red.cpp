7d67a5c67f70ca346061323bac234a6240c9ab35
GuyPerets106 <guyperets106@gmail.com>
2024-03-11 08:40:26 +0000

    [NerlWorker] Some fixes to AE_RED

--- src_cpp/opennnBridge/ae_red.cpp
+++ src_cpp/opennnBridge/ae_red.cpp
@@ -7,18 +7,21 @@ AeRed::AeRed(float k , float alpha)
     _alpha = alpha;
 }
 
-fTensor1DPtr AeRed::update_batch(fTensor2DPtr loss_values)
+fTensor1DPtr AeRed::update_batch(fTensor1D loss_values)
 {
-    fTensor1DPtr result = std::make_shared<fTensor1D>(loss_values->dimension(0));
-    for(int i = 0; i < loss_values->dimension(0); i++)
+    fTensor1DPtr result = std::make_shared<fTensor1D>(loss_values.size());
+    for(int i = 0; i < loss_values.size() - 1; i++)
     {
-        float val = update_sample(loss_values->data()[i]);
+        cout << "GOT HERE" << i << endl;
+        cout << "loss["<<i<<"] = " << loss_values(i) << endl;
+        float val = update_sample(loss_values(i));
         result->data()[i] = val;
     }
     return result;
 }
 
 float AeRed::update_sample(float loss_value){
+    cout << "GOT TO UPDATE SAMPLE WITH LOSS VALUE: " << loss_value << endl; // ? Why it raises a segmentation fault after this line
     _ema = _alpha * loss_value + (1 - _alpha) * _prev_ema;
     _prev_ema = _ema;
     _emad = _alpha * abs(loss_value - _ema) + (1 - _alpha) * _prev_emad;
@@ -30,8 +33,7 @@ float AeRed::update_sample(float loss_value){
         _ema_normal = loss_value;
     }
     _threshold = (_ema_event + _ema_normal) / 2; // New Threshold
-    if(loss_value > _threshold){
-        return loss_value;
-    }
+
+    if(loss_value > _threshold) return loss_value;
     else return 0;
 }
