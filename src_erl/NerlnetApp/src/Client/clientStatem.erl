0d35463ed59e3df0e1fda03cda2d6a4dbc7e50cd
GuyPerets106 <guyperets106@gmail.com>
2024-06-23 20:24:21 +0000

    WIP

--- src_erl/NerlnetApp/src/Client/clientStatem.erl
+++ src_erl/NerlnetApp/src/Client/clientStatem.erl
@@ -278,6 +278,7 @@ training(cast, In = {start_stream , Data}, State = #client_statem_state{etsRef =
 
 training(cast, In = {end_stream , Data}, State = #client_statem_state{etsRef = EtsRef}) ->
   {SourceName, _ClientName, WorkerName} = binary_to_term(Data),
+  io:format("Client ~p got end_stream from source ~p destined for ~p~n",[ClientName, SourceName, WorkerName]),
   ClientStatsEts = get(client_stats_ets),
   stats:increment_messages_received(ClientStatsEts),
   stats:increment_bytes_received(ClientStatsEts , nerl_tools:calculate_size(In)),
