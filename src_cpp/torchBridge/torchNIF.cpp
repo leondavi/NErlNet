ff855d5c2c8b94d8c1c4c9a97c485721c887fcea
leondavi <ddleon@outlook.co.il>
2024-07-24 21:31:33 +0300

    [TORCH] train/predict nif header

--- src_cpp/torchBridge/torchNIF.cpp
+++ src_cpp/torchBridge/torchNIF.cpp
@@ -3,11 +3,19 @@
 
 void* train_threaded_function(void* args)
 {
+    std::shared_ptr<dirty_thread_args>* p_thread_args_ptr = static_cast<std::shared_ptr<dirty_thread_args>*>(args);
+    std::shared_ptr<dirty_thread_args> thread_args_ptr = *p_thread_args_ptr;
+    delete p_thread_args_ptr;
 
+    // TODO implement training
 }
 
 
 void* predict_threaded_function(void* args)
 {
+    std::shared_ptr<dirty_thread_args>* p_thread_args_ptr = static_cast<std::shared_ptr<dirty_thread_args>*>(args);
+    std::shared_ptr<dirty_thread_args> thread_args_ptr = *p_thread_args_ptr;
+    delete p_thread_args_ptr;
 
+    // TODO implement prediction
 }
--- src_cpp/torchBridge/torchNIF.h
+++ src_cpp/torchBridge/torchNIF.h
@@ -30,18 +30,41 @@ static ERL_NIF_TERM train_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[
     std::shared_ptr<dirty_thread_args>* p_thread_args_ptr = new std::shared_ptr<dirty_thread_args>(std::make_shared<dirty_thread_args>());
     std::shared_ptr<dirty_thread_args> thread_args_ptr = *p_thread_args_ptr;
 
+    nifpp::str_atom tensor_type;
+    c10::ScalarType torch_dtype;
+
     thread_args_ptr->start_time = std::chrono::high_resolution_clock::now();
 
     enum{ARG_MODEL_ID, ARG_NERLTENSOR, ARG_NERLTENSOR_TYPE};
-    nifpp::str_atom tensor_type;
 
     nifpp::get_throws(env, argv[ARG_NERLTENSOR_TYPE],tensor_type);
-    assert(tensor_type == "float"); // add support for other types
     thread_args_ptr->return_tensor_type = tensor_type;
+    // extract model id
+    nifpp::get_throws(env, argv[ARG_MODEL_ID], thread_args_ptr->mid);
+    torch_dtype = nerlnet::get_torch_dtype(tensor_type);
+    // extract nerltensor
+    nifpp::get_nerltensor<float>(env, argv[ARG_NERLTENSOR], *(thread_args_ptr->data), torch_dtype);
 
-    nifpp::get_throws(env, argv[ARG_MODEL_ID], thread_args_ptr->mid); 
-
-    nifpp::get_nerltensor<float>(env, argv[ARG_NERLTENSOR], *(thread_args_ptr->data), torch::kFloat32);
+    char* thread_name = "train_thread";
+    int thread_create_status = enif_thread_create(thread_name, &(thread_args_ptr->tid), train_threaded_function, (void*) p_thread_args_ptr, NULL);
+    void** exit_code;
+    if (thread_create_status != 0)
+    {
+        LogError("failed to call enif_thread_create with train_nif");
+        nifpp::str_atom ret_status("train_nif_error");
+        return nifpp::make(env, ret_status);
+    }
+    else
+    {
+        thread_create_status = enif_thread_join(thread_args_ptr->tid, exit_code );
+        if (thread_create_status != 0)
+        {
+            LogError("failed to join with train_nif");
+            nifpp::str_atom ret_status("train_nif_error");
+            return nifpp::make(env, ret_status);
+        }
+    }
+    return nifpp::make(env, "ok");
 }
 /*
 predict_nif function is called by NIF from Erlang.
@@ -49,7 +72,44 @@ It creates a TorchTensor from input data and calls the threaded predict function
 */
 static ERL_NIF_TERM predict_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 { 
+    std::shared_ptr<dirty_thread_args>* p_thread_args_ptr = new std::shared_ptr<dirty_thread_args>(std::make_shared<dirty_thread_args>());
+    std::shared_ptr<dirty_thread_args> thread_args_ptr = *p_thread_args_ptr;
 
+    nifpp::str_atom tensor_type;
+    c10::ScalarType torch_dtype;
+
+    thread_args_ptr->start_time = std::chrono::high_resolution_clock::now();
+
+        enum{ARG_MODEL_ID, ARG_NERLTENSOR, ARG_NERLTENSOR_TYPE};
+
+    nifpp::get_throws(env, argv[ARG_NERLTENSOR_TYPE],tensor_type);
+    thread_args_ptr->return_tensor_type = tensor_type;
+    // extract model id
+    nifpp::get_throws(env, argv[ARG_MODEL_ID], thread_args_ptr->mid);
+    torch_dtype = nerlnet::get_torch_dtype(tensor_type);
+    // extract nerltensor
+    nifpp::get_nerltensor<float>(env, argv[ARG_NERLTENSOR], *(thread_args_ptr->data), torch_dtype);
+
+    char* thread_name = "predict_thread";
+    int thread_create_status = enif_thread_create(thread_name, &(thread_args_ptr->tid), predict_threaded_function, (void*) p_thread_args_ptr, NULL);
+    void** exit_code;
+    if (thread_create_status != 0)
+    {
+        LogError("failed to call enif_thread_create with predict_nif");
+        nifpp::str_atom ret_status("predict_nif_error");
+        return nifpp::make(env, ret_status);
+    }
+    else
+    {
+        thread_create_status = enif_thread_join(thread_args_ptr->tid, exit_code );
+        if (thread_create_status != 0)
+        {
+            LogError("failed to join with predict_nif");
+            nifpp::str_atom ret_status("predict_nif_error");
+            return nifpp::make(env, ret_status);
+        }
+    }
+    return nifpp::make(env, "ok");
 }
 
 static ErlNifFunc nif_funcs[] =
