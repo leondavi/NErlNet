87c5eb4fc93ab95675e4aaecf13148a4a4db7ac3
leondavi <ddleon@outlook.co.il>
2025-12-31 00:18:31 +0000

    Update autogenerated files to version 1.6.0

--- src_py/autogen/Definitions.py
+++ src_py/autogen/Definitions.py
@@ -1,8 +1,8 @@
 import subprocess
 from logger import *
 
-VERSION = "1.5.2"
-NERLNET_VERSION_TESTED_WITH = "1.5.2"
+VERSION = "1.6.0"
+NERLNET_VERSION_TESTED_WITH = "1.6.0"
 NERLNET_TMP_PATH = "/tmp/nerlnet"
 NERLNET_GRAPHVIZ_OUTPUT_DIR = f"{NERLNET_TMP_PATH}/nerlplanner"
 NERLNET_GLOBAL_PATH = "/usr/local/lib/nerlnet-lib/NErlNet"
